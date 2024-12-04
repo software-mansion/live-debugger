af0cd2b5e13fdf4cc0d6cf88a000aaa5571db732
kraleppa <krzysztof.nalepa@swmansion.com>
2024-12-04 11:48:11 +0100

    Updated tools versions

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule LiveDebugger.MixProject do
   def project do
     [
       app: :live_debugger,
-      version: "0.1.0",
-      elixir: "~> 1.16",
+      version: "0.0.1",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
