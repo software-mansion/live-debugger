e85001e1ea6fd21af65d14243eadf3a2dd302573
kraleppa <krzysztof.nalepa@swmansion.com>
2024-12-04 11:39:29 +0100

    Added hello world LiveView

---
+++ lib/live_debugger/web/hello_live.ex
@@ -0,0 +1,15 @@
+defmodule LiveDebugger.Web.HelloLive do
+  use Phoenix.LiveView
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, :current_pid, inspect(self()))}
+  end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <h1>Hello, from <%= @current_pid %></h1>
+    """
+  end
+end
