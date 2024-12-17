4da591b45f0e5b54ce49fcc4aecd16dc5a0eda44
kraleppa <krzysztof.nalepa@swmansion.com>
2024-12-17 16:10:33 +0100

    Added module discovery service

---
+++ lib/live_debugger/services/module_discovery.ex
@@ -0,0 +1,28 @@
+defmodule LiveDebugger.Services.ModuleDiscovery do
+  @live_view_behaviour Phoenix.LiveView
+  @live_component_behaviour Phoenix.LiveComponent
+
+  @spec find_live_modules() :: %{live_views: [module()], live_components: [module()]}
+  def find_live_modules() do
+    loaded_modules = :code.all_loaded()
+
+    %{
+      live_views: find_modules_by_behaviour(loaded_modules, @live_view_behaviour),
+      live_components: find_modules_by_behaviour(loaded_modules, @live_component_behaviour)
+    }
+  end
+
+  defp find_modules_by_behaviour(loaded_modules, behaviour) do
+    loaded_modules
+    |> Enum.map(fn {module, _} -> module end)
+    |> Enum.filter(&loaded?/1)
+    |> Enum.filter(&behaviour?(&1, behaviour))
+  end
+
+  defp loaded?(module), do: Code.ensure_loaded?(module)
+
+  defp behaviour?(module, behaviour_to_find) do
+    module_behaviours = module.module_info(:attributes)[:behaviour] || []
+    Enum.member?(module_behaviours, behaviour_to_find)
+  end
+end
