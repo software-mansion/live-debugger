bf99c02d5f95174321cfb3fc7ea905d7368b9753
kraleppa <krzysztof.nalepa@swmansion.com>
2025-01-07 14:02:49 +0100

    Added cid to trace

--- lib/live_debugger/structs/trace.ex
+++ lib/live_debugger/structs/trace.ex
@@ -3,7 +3,7 @@ defmodule LiveDebugger.Structs.Trace do
   This module provides a struct to represent a trace.
   """
 
-  defstruct [:module, :function, :arity, :args, :pid, :timestamp]
+  defstruct [:module, :function, :arity, :args, :pid, :cid, :timestamp]
 
   @type t() :: %__MODULE__{
           module: atom(),
@@ -11,6 +11,7 @@ defmodule LiveDebugger.Structs.Trace do
           arity: non_neg_integer(),
           args: list(),
           pid: pid(),
+          cid: struct() | nil,
           timestamp: non_neg_integer()
         }
 
@@ -25,7 +26,18 @@ defmodule LiveDebugger.Structs.Trace do
       arity: length(args),
       args: args,
       pid: pid,
+      cid: get_cid_from_args(args),
       timestamp: :os.system_time(:microsecond)
     }
   end
+
+  defp get_cid_from_args(args) do
+    args
+    |> Enum.map(&maybe_get_cid(&1))
+    |> Enum.find(fn elem -> is_struct(elem, Phoenix.LiveComponent.CID) end)
+  end
+
+  defp maybe_get_cid(%{myself: cid}), do: cid
+  defp maybe_get_cid(%{assigns: %{myself: cid}}), do: cid
+  defp maybe_get_cid(_), do: nil
 end
