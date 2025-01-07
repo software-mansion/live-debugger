6b3cb3a8f39d22abd0251ed45604acb7ed901978
Alan Guzek <alan.guzek@swmansion.com>
2025-01-07 13:47:39 +0100

    refactor collapsible component

--- lib/live_debugger/components/collapsible.ex
+++ lib/live_debugger/components/collapsible.ex
@@ -16,50 +16,58 @@ defmodule LiveDebugger.Components.Collapsible do
 
   def collapsible(assigns) do
     ~H"""
-    <div id={@id} class={@class} {@rest} {js_attributes("container", @id, @open)}>
-      <div {js_attributes("item", @id, @open)} data-open={if @open, do: "true", else: "false"}>
+    <div id={@id} class={@class} {@rest} x-data={"{ expanded: #{@open} }"}>
+      <div data-open={if @open, do: "true", else: "false"}>
         <div id={content_panel_header_id(@id)} class="flex items-center gap-1">
-          <button type="button" {js_attributes("button", @id, @open)}>
-            <.icon
-              name="hero-chevron-down-solid"
-              class={[@chevron_class, if(@open, do: "rotate-180")]}
-              {js_attributes("icon", @id, @open)}
-            />
-          </button>
+          <.icon_button open={@open} id={@id} chevron_class={@chevron_class} />
           {render_slot(@label)}
         </div>
-        <div {js_attributes("content_container", @id, @open)}>
+        <.content_container id={@id}>
           {render_slot(@inner_block)}
-        </div>
+        </.content_container>
       </div>
     </div>
     """
   end
 
-  defp js_attributes("container", _id, open), do: %{"x-data": "{ expanded: #{open} }"}
-  defp js_attributes("item", _id, _open), do: %{}
+  attr(:id, :string, required: true)
+  attr(:chevron_class, :string, required: true)
+  attr(:open, :boolean, required: true)
 
-  defp js_attributes("button", id, _open) do
-    %{
-      "x-on:click": "expanded = !expanded",
-      ":aria-expanded": "expanded",
-      "aria-controls": content_panel_id(id)
-    }
+  defp icon_button(assigns) do
+    ~H"""
+    <button
+      type="button"
+      x-on:click="expanded = !expanded"
+      aria-expanded="expanded"
+      aria-controls={content_panel_id(@id)}
+    >
+      <.icon
+        name="hero-chevron-down-solid"
+        class={[@chevron_class, if(@open, do: "rotate-180")]}
+        {%{":class": "{'rotate-180': expanded}"}}
+      />
+    </button>
+    """
   end
 
-  defp js_attributes("content_container", id, _open) do
-    %{
-      id: content_panel_id(id),
-      role: "region",
-      "aria-labelledby": content_panel_header_id(id),
-      "x-show": "expanded",
-      "x-cloak": true,
-      "x-collapse": true
-    }
-  end
+  attr(:id, :string, required: true)
+  slot(:inner_block)
 
-  defp js_attributes("icon", _id, _open), do: %{":class": "{ 'rotate-180': expanded }"}
-  defp js_attributes(_type, _id, _open), do: %{}
+  defp content_container(assigns) do
+    ~H"""
+    <div
+      id={content_panel_id(@id)}
+      role="region"
+      aria-labelledby={content_panel_header_id(@id)}
+      x-show="expanded"
+      x-cloak={true}
+      x-collapse={true}
+    >
+      {render_slot(@inner_block)}
+    </div>
+    """
+  end
 
   defp content_panel_header_id(id), do: "collapsible-header-#{id}"
   defp content_panel_id(id), do: "collapsible-content-panel-#{id}"
