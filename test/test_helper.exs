a71647f98da830b59fe2961a9e9d31fa3dd69b3d
kraleppa <krzysztof.nalepa@swmansion.com>
2025-01-14 16:57:52 +0100

    Added mock for process service

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,7 @@
 Mox.defmock(LiveDebugger.MockModuleService, for: LiveDebugger.Services.System.ModuleService)
 Application.put_env(:live_debugger, :module_service, LiveDebugger.MockModuleService)
 
+Mox.defmock(LiveDebugger.MockProcessService, for: LiveDebugger.Services.System.ProcessService)
+Application.put_env(:live_debugger, :process_service, LiveDebugger.MockProcessService)
+
 ExUnit.start()
