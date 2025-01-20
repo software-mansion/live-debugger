271468fdd9e344f354213c6f33bc42ebb791ee5b
kraleppa <krzysztof.nalepa@swmansion.com>
2025-01-20 12:08:50 +0100

    Extended callback discovery service

--- lib/live_debugger/services/callback_tracing_service.ex
+++ lib/live_debugger/services/callback_tracing_service.ex
@@ -18,57 +18,103 @@ defmodule LiveDebugger.Services.CallbackTracingService do
   alias Phoenix.LiveComponent.CID
   alias LiveDebugger.Services.TraceService
 
-  @id_prefix "lvdbg"
-
   @typedoc """
   Represents a raw trace straight from `:dbg.
   It should not be used outside of this module.
   """
   @type raw_trace :: {atom(), pid(), atom(), {atom(), atom(), [term()]}}
 
-  Code.ensure_compiled(:dbg)
-
-  if function_exported?(:dbg, :session, 2) do
-    Logger.warning("The :dbg module is available. Callback tracing will work.")
-  else
-    Logger.error("The :dbg module is not available. Callback tracing will not work.")
-  end
-
   @doc """
-  Starts a tracing session for the given monitored PID.
+  Starts a tracing for the given monitored PID.
   It sends traces to the recipient PID via message {:new_trace, trace}.
   It stores traces in an ETS table with id created by `CallbackTracingService.ets_table_id/1`.
-  """
-  @spec start_tracing(
-          socket_id :: String.t(),
-          monitored_pid :: pid(),
-          recipient_pid :: pid()
-        ) ::
-          {:ok, :dbg.session()} | {:error, term()}
-  def start_tracing(socket_id, monitored_pid, recipient_pid) do
-    with ets_table_id <- TraceService.ets_table_id(socket_id),
-         _table <- TraceService.init_ets(ets_table_id),
-         next_tuple_id <- TraceService.next_tuple_id(ets_table_id),
-         tracing_session_id <- tracing_session_id(monitored_pid),
-         tracing_session <- :dbg.session_create(tracing_session_id) do
-      :dbg.session(tracing_session, fn ->
-        do_trace(ets_table_id, monitored_pid, recipient_pid, next_tuple_id)
-      end)
 
-      {:ok, tracing_session}
+  Since session mechanism is available only in OTP >= 27 it is impossible to have multiple tracing sessions in earlier versions.
+  Because of that when debugger is used in project with OTP < 27 user cannot open more than one debugger sessions at the same time.
+  More info here https://www.erlang.org/docs/27/apps/runtime_tools/dbg#session/2
+  """
+
+  if Code.ensure_compiled(:dbg) && function_exported?(:dbg, :session, 2) do
+    @spec start_tracing(
+            socket_id :: String.t(),
+            monitored_pid :: pid(),
+            recipient_pid :: pid()
+          ) ::
+            {:ok, :dbg.session()} | {:error, term()}
+    def start_tracing(socket_id, monitored_pid, recipient_pid) do
+      with ets_table_id <- TraceService.ets_table_id(socket_id),
+           _table <- TraceService.init_ets(ets_table_id),
+           next_tuple_id <- TraceService.next_tuple_id(ets_table_id),
+           tracing_session_id <- tracing_session_id(monitored_pid),
+           tracing_session <- :dbg.session_create(tracing_session_id) do
+        :dbg.session(tracing_session, fn ->
+          do_trace(ets_table_id, monitored_pid, recipient_pid, next_tuple_id)
+        end)
+
+        {:ok, tracing_session}
+      end
+    rescue
+      err ->
+        Logger.error("Error while starting tracing: #{inspect(err)}")
+        {:error, err}
     end
-  rescue
-    err ->
-      Logger.error("Error while starting tracing session: #{inspect(err)}")
-      {:error, err}
+  else
+    @spec start_tracing(
+            socket_id :: String.t(),
+            monitored_pid :: pid(),
+            recipient_pid :: pid()
+          ) ::
+            {:ok, nil} | {:error, term()}
+    def start_tracing(socket_id, monitored_pid, recipient_pid) do
+      with :ok <- check_session_limit(),
+           ets_table_id <- TraceService.ets_table_id(socket_id),
+           _table <- TraceService.init_ets(ets_table_id),
+           next_tuple_id <- TraceService.next_tuple_id(ets_table_id) do
+        do_trace(ets_table_id, monitored_pid, recipient_pid, next_tuple_id)
+
+        {:ok, nil}
+      else
+        {:error, :session_limit} ->
+          Logger.error("Error while starting tracing: session limit reached")
+          {:error, :session_limit}
+      end
+    rescue
+      err ->
+        Logger.error("Error while starting tracing: #{inspect(err)}")
+        {:error, err}
+    end
+
+    defp check_session_limit() do
+      if LiveDebugger.Services.LiveViewDiscoveryService.debugger_live_pids() == [] do
+        :ok
+      else
+        {:error, :session_limit}
+      end
+    end
   end
 
   @doc """
-  Stops the tracing session.
+  Stops tracing.
   """
-  @spec stop_tracing_session(:dbg.session()) :: :ok
-  def stop_tracing_session(session) do
-    :dbg.session_destroy(session)
+  if Code.ensure_compiled(:dbg) && function_exported?(:dbg, :session, 2) do
+    @spec stop_tracing_session(:dbg.session()) :: :ok
+    def stop_tracing_session(session) do
+      :dbg.session_destroy(session)
+    end
+  else
+    @spec stop_tracing_session(nil) :: :ok
+    def stop_tracing_session(nil) do
+      :dbg.stop()
+    end
+  end
+
+  if Code.ensure_compiled(:dbg) && function_exported?(:dbg, :session, 2) do
+    @id_prefix "lvdbg"
+
+    defp tracing_session_id(monitored_pid) do
+      parsed_pid = monitored_pid |> :erlang.pid_to_list() |> to_string()
+      String.to_atom("#{@id_prefix}-#{parsed_pid}")
+    end
   end
 
   defp do_trace(ets_table_id, monitored_pid, recipient_pid, next_tuple_id) do
@@ -94,11 +140,6 @@ defmodule LiveDebugger.Services.CallbackTracingService do
       |> Enum.map(fn mfa -> :dbg.tp(mfa, []) end)
 
     [:dbg.tp({Phoenix.LiveView.Diff, :delete_component, 2}, []) | tracer_patterns]
-  end
-
-  defp tracing_session_id(monitored_pid) do
-    parsed_pid = monitored_pid |> :erlang.pid_to_list() |> to_string()
-    String.to_atom("#{@id_prefix}-#{parsed_pid}")
   end
 
   defp trace_handler(
