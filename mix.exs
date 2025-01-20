4f6c563380fdb89716f23d35175d16da0e86b624
kraleppa <krzysztof.nalepa@swmansion.com>
2025-01-20 09:45:05 +0100

    Changed bandit to dev only

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule LiveDebugger.MixProject do
        app: false,
        compile: false,
        sparse: "optimized"},
-      {:bandit, "~> 1.6"},
+      {:bandit, "~> 1.6", only: :dev},
       {:phoenix_live_reload, "~> 1.5", only: :dev},
       {:esbuild, "~> 0.7", only: :dev},
       {:tailwind, "~> 0.2", only: :dev},
