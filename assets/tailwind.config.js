d2202a8961c73a801a1e4c4a5cda7635857cf665
Alan Guzek <alan.guzek@swmansion.com>
2025-01-20 13:30:01 +0100

    change icons path

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -63,7 +63,7 @@ module.exports = {
     ),
     // Plugin for adding Heroicons
     plugin(function ({ matchComponents, theme }) {
-      let iconsDir = path.join(__dirname, './icons/heroicons/optimized');
+      let iconsDir = path.join(__dirname, './icons/heroicons/');
       let values = {};
       let icons = [
         ['', '/24/outline'],
