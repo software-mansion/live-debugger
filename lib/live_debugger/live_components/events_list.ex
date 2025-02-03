cc4cec90ba7d865f2bbb7b1f4bbce0f95cc07364
Alan Guzek <alan.guzek@swmansion.com>
2025-02-03 09:50:07 +0100

    Revert "Add modal component (#50)"
    This reverts commit 3a1be4a488305c0db748b2abee2319ac36f43daa.

--- lib/live_debugger/live_components/events_list.ex
+++ lib/live_debugger/live_components/events_list.ex
@@ -139,19 +139,7 @@ defmodule LiveDebugger.LiveComponents.EventsList do
         </div>
       </:label>
 
-      <div class="relative flex flex-col gap-4 overflow-x-auto h-[30vh] max-h-max overflow-y-auto border-2 border-gray-200 p-2 rounded-lg text-gray-600">
-        <.fullscreen_wrapper id={@id <> "-fullscreen"} class="absolute top-0 right-0">
-          <div class="w-full flex flex-col items-start justify-center">
-            <%= for {args, index} <- Enum.with_index(@trace.args) do %>
-              <.live_component
-                id={@id <> "-#{index}-fullscreen"}
-                module={LiveDebugger.LiveComponents.ElixirDisplay}
-                node={TermParser.term_to_display_tree(args)}
-                level={1}
-              />
-            <% end %>
-          </div>
-        </.fullscreen_wrapper>
+      <div class="flex flex-col gap-4 overflow-x-auto h-[30vh] max-h-max overflow-y-auto border-2 border-gray-200 p-2 rounded-lg text-gray-600">
         <%= for {args, index} <- Enum.with_index(@trace.args) do %>
           <.live_component
             id={@id <> "-#{index}"}
