a2eb98657e9d352f27285cc4d23b50aff09c4df5
Alan Guzek <alan.guzek@swmansion.com>
2025-02-10 14:51:18 +0100

    fix credo

--- lib/live_debugger/utils/formatters.ex
+++ lib/live_debugger/utils/formatters.ex
@@ -1,4 +1,7 @@
 defmodule LiveDebugger.Utils.Formatters do
+  @moduledoc """
+  This module provides functions to format data to be displayed in the UI.
+  """
   def module_short_name(module) when is_atom(module) do
     module
     |> Atom.to_string()
