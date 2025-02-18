cc2177bc917733d65af2339609dc0508aa5eb233
kraleppa <krzysztof.nalepa@swmansion.com>
2025-02-18 14:48:02 +0100

    Changed function name

--- lib/live_debugger/services/callback_tracing_service.ex
+++ lib/live_debugger/services/callback_tracing_service.ex
@@ -57,7 +57,7 @@ defmodule LiveDebugger.Services.CallbackTracingService do
   if @dbg_sessions_available do
     defp start_tracing_impl(socket_id, monitored_pid, recipient_pid) do
       with ets_table_id <- TraceService.ets_table_id(socket_id),
-           _table <- TraceService.init_ets(ets_table_id),
+           _table <- TraceService.maybe_init_ets(ets_table_id),
            next_tuple_id <- TraceService.next_tuple_id(ets_table_id),
            tracing_session_id <- tracing_session_id(monitored_pid),
            tracing_session <- :dbg.session_create(tracing_session_id) do
@@ -88,7 +88,7 @@ defmodule LiveDebugger.Services.CallbackTracingService do
     defp start_tracing_impl(socket_id, monitored_pid, recipient_pid) do
       with :ok <- check_session_limit(),
            ets_table_id <- TraceService.ets_table_id(socket_id),
-           _table <- TraceService.init_ets(ets_table_id),
+           _table <- TraceService.maybe_init_ets(ets_table_id),
            next_tuple_id <- TraceService.next_tuple_id(ets_table_id) do
         do_trace(ets_table_id, monitored_pid, recipient_pid, next_tuple_id)
 
