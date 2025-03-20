64afeb232bad136be385f47faf0822a6fd82784a
kraleppa <krzysztof.nalepa@swmansion.com>
2025-03-20 15:14:27 +0100

    Refactored fullscreen

--- lib/live_debugger/components.ex
+++ lib/live_debugger/components.ex
@@ -403,12 +403,12 @@ defmodule LiveDebugger.Components do
       id={@id}
       phx-hook="Fullscreen"
       class={[
-        "relative h-max w-full lg:w-max lg:min-w-[50rem] p-2 overflow-auto hidden flex-col rounded-md backdrop:bg-black backdrop:opacity-50"
+        "relative h-max w-full lg:w-max lg:min-w-[50rem] bg-surface-0-bg p-2 overflow-auto hidden flex-col rounded-md backdrop:bg-black backdrop:opacity-50"
         | List.wrap(@class)
       ]}
     >
       <div class="w-full h-12 py-auto px-3 flex justify-between items-center border-b border-default-border">
-        <div class="font-semibold text-base"><%= @title %></div>
+        <div class="font-semibold text-primary-text text-base"><%= @title %></div>
         <.icon_button
           id={"#{@id}-close"}
           phx-click={JS.dispatch("close", to: "##{@id}")}
