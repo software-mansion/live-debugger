fcdf517a3c076201863abeb99b3919bc116b66ba
Alan Guzek <alan.guzek@swmansion.com>
2025-03-21 11:47:31 +0100

    add task

--- lib/mix/tasks/live_debugger.assets.ex
+++ lib/mix/tasks/live_debugger.assets.ex
@@ -8,5 +8,20 @@ defmodule Mix.Tasks.LiveDebugger.Assets do
   def run(_) do
     Mix.Task.run("esbuild.install", ["--if-missing"])
     deps_path = Mix.Project.deps_path() |> IO.inspect()
+
+    Application.put_env(:esbuild, :version, "0.18.6")
+
+    Application.put_env(:esbuild, :live_debugger,
+      args: ~w(
+        bundle/app.js
+        --bundle
+        --target=es2020
+        --outdir=../priv/static/prod/
+      ),
+      cd: Path.expand("./live_debugger/priv/static", deps_path),
+      env: %{"NODE_PATH" => deps_path}
+    )
+
+    Mix.Task.run("esbuild", ["live_debugger"]) |> IO.inspect()
   end
 end
