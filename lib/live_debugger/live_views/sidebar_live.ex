eea2413077aed2c8d60819d18083cae6d15d6e87
kraleppa <krzysztof.nalepa@swmansion.com>
2025-03-31 16:19:49 +0200

    Added comment

--- lib/live_debugger/live_views/sidebar_live.ex
+++ lib/live_debugger/live_views/sidebar_live.ex
@@ -1,6 +1,7 @@
 defmodule LiveDebugger.LiveViews.SidebarLive do
   @moduledoc """
   This live view is responsible for displaying the sidebar of the LiveDebugger.
+  It receives events from the `ChannelDashboardLive` live to open the mobile sidebar.
   """
 
   use LiveDebuggerWeb, :live_view
