22c24d487149e724e219c946f3e96a6c6277e644
kraleppa <krzysztof.nalepa@swmansion.com>
2025-04-02 14:05:47 +0200

    Fixed clicking inside dropdown

--- assets/js/hooks/dropdown.js
+++ assets/js/hooks/dropdown.js
@@ -3,8 +3,16 @@ const Dropdown = {
     const contentId = `${this.el.id.replace('-button', '')}-content`;
     this.contentEl = document.getElementById(contentId);
 
+    function isHidden(el) {
+      return el.classList.contains('hidden');
+    }
+
+    function isClickOutside(event, el) {
+      return !el.contains(event.target);
+    }
+
     this.el.addEventListener('click', (event) => {
-      if (this.contentEl.classList.contains('hidden')) {
+      if (isHidden(this.contentEl)) {
         this.contentEl.classList.remove('hidden');
       } else {
         this.contentEl.classList.add('hidden');
@@ -13,8 +21,8 @@ const Dropdown = {
       event.stopPropagation();
     });
 
-    document.addEventListener('click', () => {
-      if (!this.contentEl.classList.contains('hidden')) {
+    document.addEventListener('click', (event) => {
+      if (!isHidden(this.contentEl) && isClickOutside(event, this.contentEl)) {
         this.contentEl.classList.add('hidden');
       }
 
