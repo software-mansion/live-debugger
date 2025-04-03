8bbd4bfbd97020e7b077631958a1b421e64c14cb
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2025-04-03 13:24:59 +0200

    Add dev app tests

---
+++ test/live_debugger/dev_app_test.exs
@@ -0,0 +1,50 @@
+defmodule LiveDebugger.DevAppTest do
+  use ExUnit.Case, async: true
+  use Wallaby.Feature
+
+  import Wallaby.Query
+  import LiveDebugger.Test.Mocks
+
+  @dev_app_url LiveDebuggerDev.Endpoint.url()
+
+  setup :unset_mocks
+
+  feature "user can visit Main page", %{session: session} do
+    session
+    |> visit(@dev_app_url)
+    |> assert_has(css("main > div > span", text: "Main [LiveView]"))
+    |> assert_has(button("increment"))
+    |> assert_has(button("update"))
+    |> assert_has(button("send"))
+  end
+
+  feature "user can visit Side page", %{session: session} do
+    session
+    |> visit(@dev_app_url <> "/side")
+    |> assert_has(css("main > div > span", text: "Side [LiveView]"))
+  end
+
+  feature "user can visit Nested page", %{session: session} do
+    session
+    |> visit(@dev_app_url <> "/nested")
+    |> assert_has(css("main > div > span", text: "Nested Live Views [LiveView]"))
+  end
+
+  feature "user can visit Messages page", %{session: session} do
+    session
+    |> visit(@dev_app_url <> "/messages")
+    |> assert_has(css("main > div > span", text: "Messages [LiveView]"))
+  end
+
+  feature "user can visit Embedded page", %{session: session} do
+    session
+    |> visit(@dev_app_url <> "/embedded")
+    |> assert_has(css("main > div > span", count: 3))
+  end
+
+  feature "user can visit EmbeddedInController page", %{session: session} do
+    session
+    |> visit(@dev_app_url <> "/embedded_in_controller")
+    |> assert_has(css("main > div > span", text: "Nested Live Views [LiveView]"))
+  end
+end
