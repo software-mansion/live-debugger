4bec61b5333bfbaae6dfee9162c04717c93f66eb
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2025-04-03 14:51:13 +0200

    Add moduledoc

--- test/support/mocks.ex
+++ test/support/mocks.ex
@@ -1,4 +1,8 @@
 defmodule LiveDebugger.Test.Mocks do
+  @moduledoc """
+  Utitlity module for enabling and disabling mocks for testing
+  """
+
   def set_mocks(_context) do
     Application.put_env(:live_debugger, :module_service, LiveDebugger.MockModuleService)
     Application.put_env(:live_debugger, :process_service, LiveDebugger.MockProcessService)
