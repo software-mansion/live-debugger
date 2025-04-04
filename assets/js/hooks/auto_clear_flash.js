c2de7b0319cfe64f82aba82db0b8671f747bde30
Alan Guzek <alan.guzek@swmansion.com>
2025-04-04 12:46:01 +0200

    remove comments

--- assets/js/hooks/auto_clear_flash.js
+++ assets/js/hooks/auto_clear_flash.js
@@ -4,19 +4,16 @@ const AutoClearFlash = {
     let hideElementAfter = 4000; // ms
     let clearFlashAfter = hideElementAfter + 500; // ms
 
-    // first hide the element
     setTimeout(() => {
       this.el.classList.add('max-sm:animate-fadeOutMobile');
       this.el.classList.add('sm:animate-fadeOut');
     }, hideElementAfter);
 
-    // then clear the flash
     this.timeOutId = setTimeout(() => {
       this.pushEvent('lv:clear-flash');
     }, clearFlashAfter);
   },
   destroyed() {
-    // clear the timeout if the element is destroyed before the timeout is reached
     clearTimeout(this.timeOutId);
   },
 };
