9e0fe8c05c5bdd6b0a0bb35d561a9d0b28a55387
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2025-04-08 12:30:51 +0200

    Add e2e test case module

--- test/live_debugger/live_views_dashboard_test.exs
+++ test/live_debugger/live_views_dashboard_test.exs
@@ -1,29 +1,22 @@
 defmodule LiveDebugger.LiveViewsDashboardTest do
-  use ExUnit.Case
-  use Wallaby.Feature
-
-  import Wallaby.Query
-
-  @dev_app_url Application.compile_env(:live_debugger, :dev_app_url)
-
-  @moduletag :e2e
+  use LiveDebugger.E2ECase
 
   @sessions 3
   feature "user can see active live views and refresh to see more", %{
-    sessions: [dev_session1, dev_session2, lvdbg_session]
+    sessions: [dev_app1, dev_app2, debugger]
   } do
-    dev_session1
+    dev_app1
     |> visit(@dev_app_url)
 
-    lvdbg_session
+    debugger
     |> visit("/")
     |> assert_has(title(text: "Active LiveViews"))
     |> assert_has(live_sessions(count: 1))
 
-    dev_session2
+    dev_app2
     |> visit(@dev_app_url)
 
-    lvdbg_session
+    debugger
     |> assert_has(live_sessions(count: 1))
     |> click(button("refresh"))
     |> assert_has(live_sessions(count: 2))
