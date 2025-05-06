832803ae2fccb3736ee0040ee9e759ce012f7f01
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2025-05-06 16:16:02 +0200

    Filter new traces

--- lib/live_debugger_web/live/traces_live.ex
+++ lib/live_debugger_web/live/traces_live.ex
@@ -244,11 +244,20 @@ defmodule LiveDebuggerWeb.TracesLive do
   def handle_info({:updated_trace, trace}, socket) when socket.assigns.trace_callback_running? do
     trace_display = TraceDisplay.from_trace(trace, true)
 
-    socket
+    execution_time = get_execution_times(socket)
+    min_time = Keyword.get(execution_time, :exec_time_min, 0)
+    max_time = Keyword.get(execution_time, :exec_time_max, :infinity)
+
+    if trace.execution_time >= min_time and trace.execution_time <= max_time do
+      socket
+      |> stream_insert(:existing_traces, trace_display, at: 0, limit: @live_stream_limit)
+    else
+      socket
+      |> stream_delete(:existing_traces, trace_display)
+    end
     |> assign(trace_callback_running?: false)
     |> TracingHelper.maybe_disable_tracing_after_update()
     |> push_event("stop-timer", %{})
-    |> stream_insert(:existing_traces, trace_display, at: 0, limit: @live_stream_limit)
     |> noreply()
   end
 
