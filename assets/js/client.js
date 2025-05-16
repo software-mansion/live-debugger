cd35acca9dc484f5113d210b427f9793e7181448
kraleppa <krzysztof.nalepa@swmansion.com>
2025-05-16 16:01:23 +0200

    Added window id

--- assets/js/client.js
+++ assets/js/client.js
@@ -63,6 +63,19 @@ function getSessionURL(baseURL) {
   return `${baseURL}/${session_path}`;
 }
 
+function uuidv4() {
+  return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+    (
+      +c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
+    ).toString(16)
+  );
+}
+
+if (!sessionStorage.getItem('live-debugger-window-id')) {
+  sessionStorage.setItem('live-debugger-window-id', uuidv4());
+}
+
 window.document.addEventListener('DOMContentLoaded', function () {
   const baseURL = getLiveDebuggerBaseURL();
   const sessionURL = getSessionURL(baseURL);
