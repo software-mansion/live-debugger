0cec2477681fd10bba99bda0b6de8a68d3a9afc7
kraleppa <krzysztof.nalepa@swmansion.com>
2025-05-26 15:51:37 +0200

    Added navbar and stuff

--- lib/live_debugger_web/live/settings_live.ex
+++ lib/live_debugger_web/live/settings_live.ex
@@ -5,11 +5,28 @@ defmodule LiveDebuggerWeb.SettingsLive do
 
   use LiveDebuggerWeb, :live_view
 
+  alias LiveDebuggerWeb.Components.Navbar
+  alias LiveDebuggerWeb.Helpers.RoutesHelper
+
   @impl true
   def render(assigns) do
     ~H"""
-    <div>
-      <h1>Settings</h1>
+    <div class="flex-1 min-w-[25rem] grid grid-rows-[auto_1fr]">
+      <Navbar.navbar class="flex">
+        <Navbar.return_link link={RoutesHelper.live_views_dashboard()} />
+        <Navbar.live_debugger_logo />
+      </Navbar.navbar>
+      <div class="flex-1 max-lg:p-8 pt-8 lg:w-[60rem] lg:m-auto">
+        <div class="flex items-center justify-between">
+          <.h1>Settings</.h1>
+        </div>
+
+        <div class="mt-6">
+          <div id="live-sessions" class="flex flex-col gap-4">
+            <div class="p-4 bg-surface-0-bg rounded shadow-custom border border-default-border"></div>
+          </div>
+        </div>
+      </div>
     </div>
     """
   end
