199b171b28c0f8ae5aaba9668947ff2a34a25a69
kraleppa <krzysztof.nalepa@swmansion.com>
2025-05-26 13:04:01 +0200

    Fixed navbar

--- lib/live_debugger_web/live/channel_dashboard_live.ex
+++ lib/live_debugger_web/live/channel_dashboard_live.ex
@@ -32,7 +32,7 @@ defmodule LiveDebuggerWeb.ChannelDashboardLive do
   def render(assigns) do
     ~H"""
     <div id="channel-dashboard" class="w-screen h-screen grid grid-rows-[auto_1fr]">
-      <Navbar.navbar class="grid grid-cols-[auto_auto_1fr_auto_auto] pl-2 pr-0 lg:pr-4">
+      <Navbar.navbar class="grid grid-cols-[auto_auto_1fr_auto] pl-2 lg:pr-4">
         <Navbar.return_link
           return_link={get_return_link(@lv_process, @in_iframe?)}
           class="hidden sm:block"
@@ -42,6 +42,12 @@ defmodule LiveDebuggerWeb.ChannelDashboardLive do
           class="sm:hidden"
         />
         <Navbar.live_debugger_logo_icon />
+        <div
+          :if={not @lv_process.ok?}
+          class="animate-pulse w-36 bg-surface-1-bg rounded text-surface-1-bg"
+        >
+          Loading...
+        </div>
         <Navbar.connected
           :if={@lv_process.ok?}
           id="navbar-connected"
@@ -50,7 +56,7 @@ defmodule LiveDebuggerWeb.ChannelDashboardLive do
         />
         <div class="flex items-center gap-2">
           <Navbar.theme_toggle />
-          <span class="h-5 border-r border-default-border lg:hidden"></span>
+          <span :if={@lv_process.ok?} class="h-5 border-r border-default-border lg:hidden"></span>
           <.nav_icon
             :if={@lv_process.ok?}
             phx-click={JS.push("open-sidebar", target: "#sidebar")}
