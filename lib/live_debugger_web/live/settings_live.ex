649445092a4cb61ff999f2d3915884d48577c57e
kraleppa <krzysztof.nalepa@swmansion.com>
2025-05-27 11:49:43 +0200

    Refactored settings

--- lib/live_debugger_web/live/settings_live.ex
+++ lib/live_debugger_web/live/settings_live.ex
@@ -21,7 +21,9 @@ defmodule LiveDebuggerWeb.SettingsLive do
           <.h1>Settings</.h1>
         </div>
 
+        <%!-- Upper section --%>
         <div class="mt-6 bg-surface-0-bg rounded shadow-custom border border-default-border">
+          <%!-- Appearance --%>
           <div class="p-6">
             <p class="font-semibold	mb-3">Appearance</p>
             <div class="flex gap-2">
@@ -29,52 +31,37 @@ defmodule LiveDebuggerWeb.SettingsLive do
               <.light_mode_button />
             </div>
           </div>
+          <%!-- Checkboxes --%>
           <div class="p-6 border-t border-default-border flex flex-col gap-3">
-            <div class="flex items-center">
-              <.toggle_switch
-                checked={false}
-                wrapper_class="pr-3 py-0"
-                phx-click="update"
-                phx-value-setting="deadview_mode"
-              />
-              <div class="flex flex-col gap-0.5">
-                <p class="font-semibold">Enable DeadView mode</p>
-                <p class="text-secondary-text">
-                  When enabled, LiveDebugger won't redirect to new LiveView after page redirect or reload, allowing you to browse assigns and traces of dead LiveViews.
-                </p>
-              </div>
-            </div>
-            <div class="flex items-center">
-              <.toggle_switch
-                checked={false}
-                wrapper_class="pr-3 py-0"
-                phx-click="update"
-                phx-value-setting="global_tracing"
-              />
-              <div class="flex flex-col gap-0.5">
-                <p class="font-semibold">Enable global tracing</p>
-                <p class="text-secondary-text">
-                  Enabling this feature may have a negative impact on application performance.
-                </p>
-              </div>
-            </div>
-            <div class="flex items-center">
-              <.toggle_switch
-                checked={false}
-                wrapper_class="pr-3 py-0"
-                phx-click="update"
-                phx-value-setting="refresh_tracing_on_reload"
-              />
-              <div class="flex flex-col gap-0.5">
-                <p class="font-semibold">Refresh tracing on reload</p>
-                <p class="text-secondary-text">
-                  Enabling this feature may have a negative impact on application performance.
-                </p>
-              </div>
-            </div>
+            <.settings_switch
+              label="Enable DeadView mode"
+              description="When enabled, LiveDebugger won't redirect to new LiveView after page redirect or reload, allowing you to browse assigns and traces of dead LiveViews."
+              checked={false}
+              phx-click="update"
+              phx-value-setting="deadview_mode"
+            />
+
+            <.settings_switch
+              label="Enable global tracing"
+              description="Enabling this feature may have a negative impact on application performance."
+              checked={false}
+              phx-click="update"
+              phx-value-setting="global_tracing"
+            />
+
+            <.settings_switch
+              label="Refresh tracing on reload"
+              description="Enabling this feature may have a negative impact on application performance."
+              checked={false}
+              phx-click="update"
+              phx-value-setting="refresh_tracing_on_reload"
+            />
           </div>
         </div>
+
+        <%!-- Lower section --%>
         <div class="mt-6 bg-surface-0-bg rounded shadow-custom border border-default-border">
+          <%!-- Restart button --%>
           <div class="p-6 flex flex-col md:flex-row justify-between md:items-center gap-4">
             <div class="flex flex-col gap-1">
               <p class="font-semibold">Restart LiveDebugger</p>
@@ -98,6 +85,29 @@ defmodule LiveDebuggerWeb.SettingsLive do
   @impl true
   def handle_event("restart", _, socket) do
     {:noreply, socket}
+  end
+
+  attr(:label, :string, required: true)
+  attr(:description, :string, required: true)
+  attr(:checked, :boolean, default: false)
+  attr(:rest, :global)
+
+  defp settings_switch(assigns) do
+    ~H"""
+    <div class="flex items-center">
+      <.toggle_switch
+        checked={@checked}
+        wrapper_class="pr-3 py-0"
+        phx-click="update"
+        phx-value-setting="deadview_mode"
+        {@rest}
+      />
+      <div class="flex flex-col gap-0.5">
+        <p class="font-semibold"><%= @label %></p>
+        <p class="text-secondary-text"><%= @description %></p>
+      </div>
+    </div>
+    """
   end
 
   defp dark_mode_button(assigns) do
