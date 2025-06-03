f6c611e87d9ea2975fd264fd661158fdb2306042
kraleppa <krzysztof.nalepa@swmansion.com>
2025-06-03 11:22:08 +0200

    Extracted new_traces

--- lib/live_debugger_web/live/nested/traces_live.ex
+++ lib/live_debugger_web/live/nested/traces_live.ex
@@ -13,9 +13,7 @@ defmodule LiveDebuggerWeb.Live.Nested.TracesLive do
   import LiveDebuggerWeb.Hooks.Traces.ExistingTraces
 
   alias LiveDebuggerWeb.Helpers.TracingHelper
-  alias LiveDebugger.Structs.TraceDisplay
   alias LiveDebugger.Utils.PubSub, as: PubSubUtils
-  alias LiveDebugger.Utils.Parsers
   alias LiveDebuggerWeb.Components.Traces
 
   @live_stream_limit 128
@@ -75,6 +73,7 @@ defmodule LiveDebuggerWeb.Live.Nested.TracesLive do
     |> assign(:trace_callback_running?, false)
     |> TracingHelper.init()
     |> LiveDebuggerWeb.Hooks.Traces.ExistingTraces.attach_hook(@page_size)
+    |> LiveDebuggerWeb.Hooks.Traces.NewTraces.attach_hook(@live_stream_limit)
     |> ok()
   end
 
@@ -118,59 +117,6 @@ defmodule LiveDebuggerWeb.Live.Nested.TracesLive do
     </div>
     """
   end
-
-  @impl true
-  def handle_info({:new_trace, trace}, socket) do
-    socket
-    |> TracingHelper.check_fuse()
-    |> case do
-      {:ok, socket} ->
-        trace_display = TraceDisplay.from_trace(trace, true)
-
-        socket
-        |> stream_insert(:existing_traces, trace_display, at: 0, limit: @live_stream_limit)
-        |> assign(traces_empty?: false)
-        |> assign(trace_callback_running?: true)
-
-      {:stopped, socket} ->
-        limit = TracingHelper.trace_limit_per_period()
-        period = TracingHelper.time_period() |> Parsers.parse_elapsed_time()
-
-        socket.assigns.parent_pid
-        |> push_flash(
-          socket,
-          "Callback tracer stopped: Too many callbacks in a short time. Current limit is #{limit} callbacks in #{period}."
-        )
-
-      {_, socket} ->
-        socket
-    end
-    |> noreply()
-  end
-
-  @impl true
-  def handle_info({:updated_trace, trace}, socket) when socket.assigns.trace_callback_running? do
-    trace_display = TraceDisplay.from_trace(trace, true)
-
-    execution_time = get_execution_times(socket)
-    min_time = Keyword.get(execution_time, :exec_time_min, 0)
-    max_time = Keyword.get(execution_time, :exec_time_max, :infinity)
-
-    if trace.execution_time >= min_time and trace.execution_time <= max_time do
-      socket
-      |> stream_insert(:existing_traces, trace_display, at: 0, limit: @live_stream_limit)
-    else
-      socket
-      |> stream_delete(:existing_traces, trace_display)
-    end
-    |> assign(trace_callback_running?: false)
-    |> TracingHelper.maybe_disable_tracing_after_update()
-    |> push_event("stop-timer", %{})
-    |> noreply()
-  end
-
-  @impl true
-  def handle_info({:updated_trace, _trace}, socket), do: {:noreply, socket}
 
   @impl true
   def handle_info({:params_changed, new_params}, socket) do
