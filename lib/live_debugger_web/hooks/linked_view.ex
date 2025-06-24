52a7df8178d2b1f50e980ccfee536764ba792e27
Krzysztof Nalepa <56135216+kraleppa@users.noreply.github.com>
2025-06-24 15:44:14 +0200

    Bug: add missing spinner on successor finding (#514)

--- lib/live_debugger_web/hooks/linked_view.ex
+++ lib/live_debugger_web/hooks/linked_view.ex
@@ -83,11 +83,23 @@ defmodule LiveDebuggerWeb.Hooks.LinkedView do
     |> halt()
   end
 
+  def handle_async(:find_successor, {:ok, %{pid: successor_pid}}, socket) do
+    socket
+    |> push_navigate(to: RoutesHelper.channel_dashboard(successor_pid))
+    |> halt()
+  end
+
+  def handle_async(:find_successor, _, socket) do
+    socket
+    |> push_navigate(to: RoutesHelper.error("unexpected_error"))
+    |> halt()
+  end
+
   def handle_async(_, _, socket), do: {:cont, socket}
 
   def handle_info(:find_successor, socket) do
     socket
-    |> find_successor_lv_process()
+    |> async_find_successor_lv_process()
     |> halt()
   end
 
@@ -114,6 +126,20 @@ defmodule LiveDebuggerWeb.Hooks.LinkedView do
   end
 
   def handle_info(_, socket), do: {:cont, socket}
+
+  defp async_find_successor_lv_process(
+         %{assigns: %{lv_process: %{result: %LvProcess{} = lv_process}}} = socket
+       ) do
+    socket
+    |> assign(:lv_process, AsyncResult.loading())
+    |> start_async(:find_successor, fn ->
+      fetch_with_retries(fn -> LiveViewDiscoveryService.successor_lv_process(lv_process) end)
+    end)
+  end
+
+  defp async_find_successor_lv_process(socket) do
+    push_navigate(socket, to: RoutesHelper.error("not_found"))
+  end
 
   defp find_successor_lv_process(socket) do
     with %{lv_process: %{result: %LvProcess{} = lv_process}} <- socket.assigns,
