eef7ecc110bca073cf07451436078f2f7bf6fee9
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2025-07-07 13:04:00 +0200

    Add test for LiveDebugger.API.System.Module.Impl

---
+++ test/test_refactor/api/system/module_test.ex
@@ -0,0 +1,24 @@
+defmodule LiveDebugger.API.System.ModuleImplTest do
+  use ExUnit.Case, async: true
+
+  alias LiveDebugger.API.System.Module.Impl, as: ModuleImpl
+
+  describe "behaviours/1" do
+    defmodule TestLiveViewModule do
+      use Phoenix.LiveView
+    end
+
+    defmodule TestLiveComponentModule do
+      use Phoenix.LiveComponent
+    end
+
+    defmodule TestNoBehaviourModule do
+    end
+
+    test "returns correct behaviours" do
+      assert Phoenix.LiveView in ModuleImpl.behaviours(TestLiveViewModule)
+      assert Phoenix.LiveComponent in ModuleImpl.behaviours(TestLiveComponentModule)
+      assert [] = ModuleImpl.behaviours(TestNoBehaviourModule)
+    end
+  end
+end
