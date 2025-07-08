2566f7d7fb29bc02f86eecaa1cad66e619a2def8
Alan Guzek <alan.guzek@swmansion.com>
2025-07-08 12:33:54 +0200

    Refactor: Add api for `:dbg` module (#566)
    * add api
    
    * add tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -36,6 +36,9 @@ else
   Mox.defmock(LiveDebugger.MockSettingsServer, for: LiveDebugger.GenServers.SettingsServer)
   Application.put_env(:live_debugger, :settings_server, LiveDebugger.MockSettingsServer)
 
+  Mox.defmock(LiveDebugger.MockAPIDbg, for: LiveDebuggerRefactor.API.System.Dbg)
+  Application.put_env(:live_debugger, :api_dbg, LiveDebugger.MockAPIDbg)
+
   Mox.defmock(LiveDebuggerRefactor.MockAPIModule, for: LiveDebuggerRefactor.API.System.Module)
   Application.put_env(:live_debugger, :api_module, LiveDebuggerRefactor.MockAPIModule)
 
