ea5a953d9c061c79dec921718c065460943e4cbd
Krzysztof Nalepa <56135216+kraleppa@users.noreply.github.com>
2025-07-08 11:21:26 +0200

    Refactor: add event behavior (#567)
    * Added event module
    
    * Added better docs
    
    * Added docs
    
    * Added event type
    
    * Added README to live_debugger_refactor
    
    * Added basic tests
    
    * Fixed credo

---
+++ lib/live_debugger_refactor/README.md
@@ -0,0 +1,3 @@
+### LiveDebuggerRefactor
+
+In this module we put refactored modules that are currently dead and not used by LiveDebugger app. You shouldn't care about modules in this directory, unless you want to contribute to refactor.
---
+++ test/live_debugger_refactor/event.ex
@@ -0,0 +1,35 @@
+defmodule LiveDebuggerRefactor.EventTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+
+  defmodule TestEvents do
+    @moduledoc false
+    use LiveDebuggerRefactor.Event
+
+    defevent(UserCreated, name: String.t(), email: String.t(), age: integer())
+  end
+
+  test "generates a struct with the correct fields with default context" do
+    event = %TestEvents.UserCreated{name: "John", email: "john@example.com", age: 30}
+
+    assert event.name == "John"
+    assert event.email == "john@example.com"
+    assert event.age == 30
+    assert event.context == %{}
+  end
+
+  test "generates a struct with the correct fields with custom context" do
+    event = %TestEvents.UserCreated{
+      name: "John",
+      email: "john@example.com",
+      age: 30,
+      context: %{pid: self()}
+    }
+
+    assert event.name == "John"
+    assert event.email == "john@example.com"
+    assert event.age == 30
+    assert event.context == %{pid: self()}
+  end
+end
