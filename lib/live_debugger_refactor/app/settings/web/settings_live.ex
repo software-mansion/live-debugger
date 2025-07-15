0915e63db30f569985681012d209c8051148bad5
Alan Guzek <alan.guzek@swmansion.com>
2025-07-15 13:19:37 +0200

    prepare main contexts

---
+++ lib/live_debugger_refactor/app/settings/web/settings_live.ex
@@ -0,0 +1,9 @@
+defmodule LiveDebuggerRefactor.App.Settings.Web.SettingsLive do
+  @moduledoc false
+
+  use LiveDebuggerRefactor.App.Web, :live_view
+
+  def render(assigns) do
+    ~H""
+  end
+end
