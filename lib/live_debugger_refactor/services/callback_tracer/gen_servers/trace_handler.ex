0645bc700e3659eb67c3047c0ba7d0ae249ecc63
kraleppa <krzysztof.nalepa@swmansion.com>
2025-07-17 11:32:37 +0200

    Basic trace pattern matching added

--- lib/live_debugger_refactor/services/callback_tracer/gen_servers/trace_handler.ex
+++ lib/live_debugger_refactor/services/callback_tracer/gen_servers/trace_handler.ex
@@ -5,6 +5,12 @@ defmodule LiveDebuggerRefactor.Services.CallbackTracer.GenServers.TraceHandler d
 
   use GenServer
 
+  require Logger
+
+  alias LiveDebuggerRefactor.Utils.Callbacks, as: CallbackUtils
+
+  @callback_functions Enum.map(CallbackUtils.all_callbacks(), &elem(&1, 0)) |> dbg()
+
   @spec start_link(opts :: list()) :: GenServer.on_start()
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -25,8 +31,74 @@ defmodule LiveDebuggerRefactor.Services.CallbackTracer.GenServers.TraceHandler d
 
   @impl true
   def handle_cast({:new_trace, trace, n}, state) do
-    dbg(trace)
-    dbg(n)
+    handle_trace(trace, n)
+
     {:noreply, state}
+  end
+
+  # We catch this trace to know when modules were recompiled.
+  # We do not display this trace to user, so we do not have to care about order
+  # We need to catch that case because tracer disconnects from modules that were recompiled
+  # and we need to reapply tracing patterns to them.
+  # This will be replaced in the future with a more efficient way to handle this.
+  # https://github.com/software-mansion/live-debugger/issues/592
+  defp handle_trace({_, _, :return_from, {Mix.Tasks.Compile.Elixir, _, _}, {:ok, _}, _}, n) do
+    # TODO: Update traced modules
+    dbg("Update traced modules")
+
+    :ok
+  end
+
+  defp handle_trace({_, _, _, {Mix.Tasks.Compile.Elixir, _, _}, _}, _), do: :ok
+  defp handle_trace({_, _, _, {Mix.Tasks.Compile.Elixir, _, _}, _, _}, _), do: :ok
+
+  # We do not display this trace to user, so we do not have to care about order
+  # We trace it to refresh the components tree
+  # It will be replaced in the future by telemetry event added in LiveView 1.1.0
+  # [:phoenix, :live_component, :destroyed]
+  # https://hexdocs.pm/phoenix_live_view/1.1.0-rc.3/telemetry.html
+  defp handle_trace(
+         {_, pid, _, {Phoenix.LiveView.Diff, :delete_component, [cid | _] = args}, timestamp},
+         n
+       ) do
+    dbg("Delete component")
+    dbg(pid)
+    dbg(cid)
+    dbg(args)
+    dbg(timestamp)
+
+    :ok
+  end
+
+  # This handles callbacks created by user that will be displayed to user
+  # It cannot be async because we care about order
+  defp handle_trace({_, pid, :call, {module, fun, args}, timestamp}, n)
+       when fun in @callback_functions do
+    dbg("Callback")
+    dbg(pid)
+    dbg(module)
+    dbg(fun)
+    dbg(args)
+    dbg(timestamp)
+
+    :ok
+  end
+
+  # This handles callbacks created by user that will be displayed to user
+  # It cannot be async because we care about order
+  defp handle_trace({_, pid, type, {module, fun, _arity}, _, return_ts}, n)
+       when fun in @callback_functions and type in [:return_from, :exception_from] do
+    dbg("Callback return")
+    dbg(pid)
+    dbg(module)
+    dbg(fun)
+    dbg(return_ts)
+
+    :ok
+  end
+
+  defp handle_trace(trace, n) do
+    Logger.info("Ignoring unexpected trace: #{inspect(trace)}")
+    n
   end
 end
