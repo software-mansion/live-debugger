6e771473c5f09e2f3028698c8e48e06ebd4d674c
kraleppa <krzysztof.nalepa@swmansion.com>
2025-07-17 12:28:47 +0200

    Changed handlers

--- lib/live_debugger_refactor/services/callback_tracer/gen_servers/trace_handler.ex
+++ lib/live_debugger_refactor/services/callback_tracer/gen_servers/trace_handler.ex
@@ -11,8 +11,6 @@ defmodule LiveDebuggerRefactor.Services.CallbackTracer.GenServers.TraceHandler d
 
   @allowed_callbacks Enum.map(CallbackUtils.all_callbacks(), &elem(&1, 0))
 
-  defguard allowed?(fun) when fun in @allowed_callbacks
-
   @spec start_link(opts :: list()) :: GenServer.on_start()
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -32,89 +30,61 @@ defmodule LiveDebuggerRefactor.Services.CallbackTracer.GenServers.TraceHandler d
   end
 
   @impl true
-  def handle_cast({:new_trace, trace, n}, state) do
-    handle_trace(trace, n)
+  def handle_cast(
+        {:new_trace, {_, _, :return_from, {Mix.Tasks.Compile.Elixir, _, _}, {:ok, _}, _}, n},
+        state
+      ) do
+    # TODO: Update traced modules
+    dbg("Update traced modules")
+    dbg([n, state])
 
     {:noreply, state}
   end
 
-  # We catch this trace to know when modules were recompiled.
-  # We do not display this trace to user, so we do not have to care about order
-  # We need to catch that case because tracer disconnects from modules that were recompiled
-  # and we need to reapply tracing patterns to them.
-  # This will be replaced in the future with a more efficient way to handle this.
-  # https://github.com/software-mansion/live-debugger/issues/592
-  defp handle_trace({_, _, :return_from, {Mix.Tasks.Compile.Elixir, _, _}, {:ok, _}, _}, n) do
-    # TODO: Update traced modules
-    dbg("Update traced modules")
-    dbg(n)
-
-    :ok
+  @impl true
+  def handle_cast({:new_trace, {_, _, _, {Mix.Tasks.Compile.Elixir, _, _}, _}, _}, state) do
+    {:noreply, state}
   end
 
-  defp handle_trace({_, _, _, {Mix.Tasks.Compile.Elixir, _, _}, _}, _), do: :ok
-  defp handle_trace({_, _, _, {Mix.Tasks.Compile.Elixir, _, _}, _, _}, _), do: :ok
+  @impl true
+  def handle_cast({:new_trace, {_, _, _, {Mix.Tasks.Compile.Elixir, _, _}, _, _}, _}, state) do
+    {:noreply, state}
+  end
 
-  # We do not display this trace to user, so we do not have to care about order
-  # We trace it to refresh the components tree
-  # It will be replaced in the future by telemetry event added in LiveView 1.1.0
-  # [:phoenix, :live_component, :destroyed]
-  # https://hexdocs.pm/phoenix_live_view/1.1.0-rc.3/telemetry.html
-  defp handle_trace(
-         {_, pid, _, {Phoenix.LiveView.Diff, :delete_component, [cid | _] = args}, ts},
-         n
-       ) do
+  @impl true
+  def handle_cast(
+        {:new_trace,
+         {_, pid, _, {Phoenix.LiveView.Diff, :delete_component, [cid | _] = args}, ts}, n},
+        state
+      ) do
     dbg("Delete component")
-    dbg(pid)
-    dbg(cid)
-    dbg(args)
-    dbg(ts)
-    dbg(n)
+    dbg([pid, cid, args, ts, n, state])
 
-    :ok
+    {:noreply, state}
   end
 
-  # This handles callbacks created by user that will be displayed to user
-  # It cannot be async because we care about order
-  defp handle_trace({_, pid, :call, {module, fun, args}, ts}, n) when allowed?(fun) do
+  @impl true
+  def handle_cast({:new_trace, {_, pid, :call, {module, fun, args}, ts}, n}, state)
+      when fun in @allowed_callbacks do
     dbg("Callback")
-    dbg(pid)
-    dbg(module)
-    dbg(fun)
-    dbg(args)
-    dbg(ts)
-    dbg(n)
+    dbg([pid, module, fun, args, ts, n, state])
 
-    :ok
+    {:noreply, state}
   end
 
-  # This handles callbacks created by user that will be displayed to user
-  # It cannot be async because we care about order
-  defp handle_trace({_, pid, :return_from, {module, fun, _}, _, ts}, n) when allowed?(fun) do
-    dbg("Callback return")
-    dbg(pid)
-    dbg(module)
-    dbg(fun)
-    dbg(ts)
-    dbg(n)
-    :ok
+  @impl true
+  def handle_cast({:new_trace, {_, pid, type, {module, fun, _}, _, ts}, n}, state)
+      when fun in @allowed_callbacks and type in [:return_from, :exception_from] do
+    dbg("Callback #{type}")
+    dbg([pid, module, fun, ts, n, state])
+
+    {:noreply, state}
   end
 
-  # This handles callbacks created by user that will be displayed to user
-  # It cannot be async because we care about order
-  defp handle_trace({_, pid, :exception_from, {module, fun, _}, _, ts}, n) when allowed?(fun) do
-    dbg("Callback exception")
-    dbg(pid)
-    dbg(module)
-    dbg(fun)
-    dbg(ts)
-    dbg(n)
-
-    :ok
-  end
-
-  defp handle_trace(trace, n) do
+  @impl true
+  def handle_cast({:new_trace, trace, _n}, state) do
     Logger.info("Ignoring unexpected trace: #{inspect(trace)}")
-    n
+
+    {:noreply, state}
   end
 end
