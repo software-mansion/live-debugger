3dd072908027d61a371e0e3c759723dbc57da42a
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2025-07-18 15:10:18 +0200

    Add Feature helper

---
+++ lib/live_debugger_refactor/feature.ex
@@ -0,0 +1,14 @@
+defmodule LiveDebuggerRefactor.Feature do
+  @moduledoc """
+  Feature flags for LiveDebugger.
+  If you create a new feature that can be enabled or disabled, create a new function here with defined rules for enabling it.
+  """
+
+  def enabled?(:garbage_collection) do
+    Application.get_env(:live_debugger, :garbage_collection?, true)
+  end
+
+  def enabled?(feature_name) do
+    raise "Feature #{feature_name} is not allowed"
+  end
+end
