636f20173bb01fca42672a97977597d51a8401ad
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2025-07-21 17:09:45 +0200

    Implement GarbageCollector GenServer

--- lib/live_debugger_refactor/services/garbage_collector/gen_servers/garbage_collector.ex
+++ lib/live_debugger_refactor/services/garbage_collector/gen_servers/garbage_collector.ex
@@ -5,6 +5,12 @@ defmodule LiveDebuggerRefactor.Services.GarbageCollector.GenServers.GarbageColle
 
   use GenServer
 
+  alias LiveDebuggerRefactor.Services.GarbageCollector.Actions.GarbageCollecting,
+    as: GarbageCollectingActions
+
+  alias LiveDebuggerRefactor.Bus
+  alias LiveDebuggerRefactor.Services.GarbageCollector.Events.GarbageCollected
+
   @garbage_collect_interval 2000
 
   def start_link(opts \\ []) do
@@ -14,17 +20,22 @@ defmodule LiveDebuggerRefactor.Services.GarbageCollector.GenServers.GarbageColle
   @impl true
   def init(_opts) do
     if LiveDebuggerRefactor.Feature.enabled?(:garbage_collection) do
-      init_garbage_collection_loop()
+      Task.start(&garbage_collection_loop/0)
     end
 
     {:ok, []}
   end
 
-  @spec init_garbage_collection_loop() :: {:ok, pid()}
-  defp init_garbage_collection_loop() do
-    Task.start(fn ->
-      garbage_collection_loop()
-    end)
+  @impl true
+  def handle_call(:garbage_collect, _from, state) do
+    traces_collected = GarbageCollectingActions.garbage_collect_traces!()
+    states_collected = GarbageCollectingActions.garbage_collect_states!()
+
+    if traces_collected or states_collected do
+      Bus.broadcast_event!(%GarbageCollected{})
+    end
+
+    {:reply, :ok, state}
   end
 
   @spec garbage_collection_loop() :: no_return()
@@ -32,12 +43,5 @@ defmodule LiveDebuggerRefactor.Services.GarbageCollector.GenServers.GarbageColle
     Process.sleep(@garbage_collect_interval)
     :ok = GenServer.call(__MODULE__, :garbage_collect)
     garbage_collection_loop()
-  end
-
-  @impl true
-  def handle_call(:garbage_collect, _from, state) do
-    # TODO: Implement the garbage collection logic here.
-
-    {:reply, :ok, state}
   end
 end
