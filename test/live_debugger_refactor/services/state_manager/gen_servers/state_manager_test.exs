2fc80838f3abdd6f9280d7e0c2d25439e46bd0f1
kraleppa <krzysztof.nalepa@swmansion.com>
2025-07-22 13:19:27 +0200

    Fixed tests

--- test/live_debugger_refactor/services/state_manager/gen_servers/state_manager_test.exs
+++ test/live_debugger_refactor/services/state_manager/gen_servers/state_manager_test.exs
@@ -47,6 +47,7 @@ defmodule LiveDebuggerRefactor.Services.StateManager.GenServers.StateManagerTest
         trace_id: -1,
         function: :render,
         module: LiveDebuggerTest.DummyLiveView,
+        ets_ref: nil,
         cid: nil,
         pid: pid
       }
