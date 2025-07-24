c783363f5b35814fc9b116efc35f4d3383d0ecd6
Alan Guzek <alan.guzek@swmansion.com>
2025-07-24 12:09:56 +0200

    remove testing code

--- lib/live_debugger_refactor/app/debugger/web/debugger_live.ex
+++ lib/live_debugger_refactor/app/debugger/web/debugger_live.ex
@@ -3,14 +3,7 @@ defmodule LiveDebuggerRefactor.App.Debugger.Web.DebuggerLive do
 
   use LiveDebuggerRefactor.App.Web, :live_view
 
-  alias LiveDebuggerRefactor.App.Debugger.ComponentsTree.Web, as: ComponentsTreeWeb
-
   def render(assigns) do
-    ~H"""
-    <div id="debugger-live">
-      <h1>TEMPORARY DEBUGGER LIVE</h1>
-      <ComponentsTreeWeb.components_tree_live id="components-tree" socket={@socket} />
-    </div>
-    """
+    ~H""
   end
 end
