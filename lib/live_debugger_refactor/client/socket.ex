7ebca94066dab9a36984c27b0c96df3cd8a9a67c
kraleppa <krzysztof.nalepa@swmansion.com>
2025-07-25 11:41:08 +0200

    Add `@impl true` in `Client.Socket`

--- lib/live_debugger_refactor/client/socket.ex
+++ lib/live_debugger_refactor/client/socket.ex
@@ -5,10 +5,12 @@ defmodule LiveDebuggerRefactor.Client.Socket do
 
   channel("client:*", LiveDebuggerRefactor.Client.Channel)
 
+  @impl true
   def connect(%{"sessionId" => session_id}, socket) do
     socket = assign(socket, :debugged_socket_id, session_id)
     {:ok, socket}
   end
 
+  @impl true
   def id(socket), do: "client:#{socket.assigns.debugged_socket_id}"
 end
