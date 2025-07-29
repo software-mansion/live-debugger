5a1270b75bdc5d2096303664e21475c8042525a5
Alan Guzek <alan.guzek@swmansion.com>
2025-07-29 17:26:15 +0200

    add parsers for time data

--- test/live_debugger_refactor/app/utils/parsers_test.exs
+++ test/live_debugger_refactor/app/utils/parsers_test.exs
@@ -3,6 +3,35 @@ defmodule LiveDebuggerRefactor.App.Utils.ParsersTest do
 
   alias LiveDebuggerRefactor.App.Utils.Parsers
 
+  describe "parse_timestamp/1" do
+    test "parses a valid timestamp" do
+      timestamp1 = 1_000_000
+      assert Parsers.parse_timestamp(timestamp1) == "00:00:01.000000"
+
+      timestamp2 = 60_000_000
+      assert Parsers.parse_timestamp(timestamp2) == "00:01:00.000000"
+    end
+
+    test "returns \"Invalid timestamp\" when timestamp is invalid" do
+      timestamp = 109_201_930_129_391_238_012_983_091_283_712_097_380_127
+      assert Parsers.parse_timestamp(timestamp) == "Invalid timestamp"
+    end
+  end
+
+  describe "parse_elapsed_time/1" do
+    test "parses microseconds for less than 1ms" do
+      assert Parsers.parse_elapsed_time(500) == "500 µs"
+    end
+
+    test "parses milliseconds" do
+      assert Parsers.parse_elapsed_time(1_500) == "1 ms"
+    end
+
+    test "parses seconds" do
+      assert Parsers.parse_elapsed_time(1_500_000) == "1.50 s"
+    end
+  end
+
   test "pid_to_string/1 converts pid to string" do
     pid = :c.pid(0, 123, 0)
     assert Parsers.pid_to_string(pid) == "0.123.0"
