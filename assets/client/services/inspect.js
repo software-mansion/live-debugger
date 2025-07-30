ffb36c7667cf8fee110078db85c15440f78418f6
kraleppa <krzysztof.nalepa@swmansion.com>
2025-07-30 11:21:03 +0200

    Basic flow added

--- assets/client/services/inspect.js
+++ assets/client/services/inspect.js
@@ -3,16 +3,33 @@ export default function initElementInspection() {
 
   let inspectMode = false;
 
+  const handleInspect = () => {
+    console.log('Inspecting...');
+    disableInspectMode();
+  };
+
+  const disableInspectMode = () => {
+    if (!inspectMode) {
+      return;
+    }
+
+    inspectMode = false;
+    document.body.style.cursor = 'default';
+    window.removeEventListener('click', handleInspect);
+  };
+
   const enableInspectMode = () => {
     if (inspectMode) {
       return;
     }
 
     inspectMode = true;
+    document.body.style.cursor = 'crosshair';
+    window.addEventListener('click', handleInspect);
     console.log('Inspect mode enabled');
   };
 
   document.addEventListener('live-debugger-debug-button-inspect', (event) => {
-    enableInspectMode();
+    setTimeout(enableInspectMode);
   });
 }
