dcb461c49a766b89c10a249eabe1ac196ebe0a54
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2025-08-01 10:24:54 +0200

    Add fetching nested LiveViews

--- lib/live_debugger_refactor/app/debugger/nested_live_view_links/web/nested_live_view_links_live.ex
+++ lib/live_debugger_refactor/app/debugger/nested_live_view_links/web/nested_live_view_links_live.ex
@@ -1,13 +1,46 @@
 defmodule LiveDebuggerRefactor.App.Debugger.NestedLiveViewLinks.Web.NestedLiveViewLinksLive do
   use LiveDebuggerRefactor.App.Web, :live_view
 
-  alias Phoenix.LiveView.AsyncResult
+  alias LiveDebuggerRefactor.Structs.LvProcess
+  alias LiveDebuggerRefactor.API.LiveViewDiscovery
   alias LiveDebuggerRefactor.App.Debugger.Web.Components, as: DebuggerComponents
 
+  @doc """
+  Renders the `NodeStateLive` as a nested LiveView component.
+
+  `id` - dom id
+  `socket` - parent LiveView socket
+  `lv_process` - currently debugged LiveView process
+  """
+  attr(:id, :string, required: true)
+  attr(:socket, Phoenix.LiveView.Socket, required: true)
+  attr(:lv_process, LvProcess, required: true)
+  attr(:class, :string, default: "", doc: "CSS class for the container")
+
+  def live_render(assigns) do
+    session = %{
+      "lv_process" => assigns.lv_process,
+      "parent_pid" => self()
+    }
+
+    assigns = assign(assigns, session: session)
+
+    ~H"""
+    <%= live_render(@socket, __MODULE__,
+      id: @id,
+      session: @session,
+      container: {:div, class: @class}
+    ) %>
+    """
+  end
+
   @impl true
-  def mount(_params, _session, socket) do
+  def mount(_params, session, socket) do
+    lv_process = session["lv_process"]
+
     socket
-    |> assign(nested_lv_processes: AsyncResult.ok([]))
+    |> assign(lv_process: lv_process)
+    |> assign_async_nested_lv_processes()
     |> ok()
   end
 
@@ -35,5 +68,11 @@ defmodule LiveDebuggerRefactor.App.Debugger.NestedLiveViewLinks.Web.NestedLiveVi
       </.async_result>
     </div>
     """
+  end
+
+  defp assign_async_nested_lv_processes(%{assigns: %{lv_process: %{pid: pid}}} = socket) do
+    assign_async(socket, :nested_lv_processes, fn ->
+      {:ok, %{nested_lv_processes: LiveViewDiscovery.children_lv_processes(pid)}}
+    end)
   end
 end
