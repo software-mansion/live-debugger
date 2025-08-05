b0bc243013280ae29282a1a40c2849a2b9512d00
kraleppa <krzysztof.nalepa@swmansion.com>
2025-08-05 13:54:05 +0200

    Added events

--- lib/live_debugger_refactor/app/events.ex
+++ lib/live_debugger_refactor/app/events.ex
@@ -16,4 +16,5 @@ defmodule LiveDebuggerRefactor.App.Events do
   defevent(DebuggerMounted, debugged_pid: pid(), debugger_pid: pid())
   defevent(DebuggerTerminated, debugged_pid: pid(), debugger_pid: pid())
   defevent(ParamsChanged, params: map())
+  defevent(FindSuccessor, debugged_socket_id: String.t())
 end
---
+++ lib/live_debugger_refactor/services/successor_discoverer/events.ex
@@ -0,0 +1,9 @@
+defmodule LiveDebuggerRefactor.Services.SuccessorDiscoverer.Events do
+  @moduledoc """
+  Events for the `SuccessorDiscoverer` service.
+  """
+
+  use LiveDebuggerRefactor.Event
+
+  defevent(SuccessorFound, old_socket_id: String.t(), new_socket_id: String.t())
+end
