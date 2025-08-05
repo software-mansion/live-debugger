901ac4f067d72a2ba264b3de7b28350f5f5c626e
kraleppa <krzysztof.nalepa@swmansion.com>
2025-08-05 13:54:52 +0200

    Handling FindSuccessor event

--- lib/live_debugger_refactor/services/successor_discoverer/gen_servers/client_events_receiver.ex
+++ lib/live_debugger_refactor/services/successor_discoverer/gen_servers/client_events_receiver.ex
@@ -7,6 +7,10 @@ defmodule LiveDebuggerRefactor.Services.SuccessorDiscoverer.GenServers.ClientEve
 
   alias LiveDebuggerRefactor.Client
 
+  alias LiveDebuggerRefactor.Bus
+  alias LiveDebuggerRefactor.App.Events.FindSuccessor
+  alias LiveDebuggerRefactor.Services.SuccessorDiscoverer.Events.SuccessorFound
+
   defmodule State do
     @moduledoc """
     State of `SuccessorDiscoverer` service.
@@ -26,6 +30,7 @@ defmodule LiveDebuggerRefactor.Services.SuccessorDiscoverer.GenServers.ClientEve
   @impl true
   def init(_opts) do
     Client.receive_events()
+    Bus.receive_events()
 
     {:ok, %State{}}
   end
@@ -41,10 +46,34 @@ defmodule LiveDebuggerRefactor.Services.SuccessorDiscoverer.GenServers.ClientEve
         |> put_window_to_socket(window_id, socket_id)
         |> put_socket_to_window(socket_id, window_id)
 
-      {:noreply, new_state |> dbg()}
+      {:noreply, new_state}
     else
       {:noreply, state}
     end
+  end
+
+  @impl true
+  def handle_info(%FindSuccessor{debugged_socket_id: old_socket_id}, state) do
+    window_id = get_window_id(state, old_socket_id)
+    new_socket_id = get_socket_id(state, window_id)
+
+    if is_binary(new_socket_id) do
+      new_state = remove_socket_from_window(state, old_socket_id)
+
+      Bus.broadcast_event!(%SuccessorFound{
+        old_socket_id: old_socket_id,
+        new_socket_id: new_socket_id
+      })
+
+      {:noreply, new_state}
+    else
+      {:noreply, state}
+    end
+  end
+
+  @impl true
+  def handle_info(_, state) do
+    {:noreply, state}
   end
 
   defp put_window_to_socket(state, window_id, socket_id) do
@@ -53,5 +82,17 @@ defmodule LiveDebuggerRefactor.Services.SuccessorDiscoverer.GenServers.ClientEve
 
   defp put_socket_to_window(state, socket_id, window_id) do
     %{state | socket_to_window: Map.put(state.socket_to_window, socket_id, window_id)}
+  end
+
+  defp get_window_id(state, socket_id) do
+    state.socket_to_window[socket_id]
+  end
+
+  defp get_socket_id(state, window_id) do
+    state.window_to_socket[window_id]
+  end
+
+  defp remove_socket_from_window(state, socket_id) do
+    %{state | socket_to_window: Map.delete(state.socket_to_window, socket_id)}
   end
 end
