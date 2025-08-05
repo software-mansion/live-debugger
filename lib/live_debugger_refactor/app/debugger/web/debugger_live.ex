06c7f01cd66eaf460acb917ce6f54b56785369f3
Alan Guzek <alan.guzek@swmansion.com>
2025-08-05 13:13:24 +0200

    TEMP! add code to see the nested live views

--- lib/live_debugger_refactor/app/debugger/web/debugger_live.ex
+++ lib/live_debugger_refactor/app/debugger/web/debugger_live.ex
@@ -3,7 +3,50 @@ defmodule LiveDebuggerRefactor.App.Debugger.Web.DebuggerLive do
 
   use LiveDebuggerRefactor.App.Web, :live_view
 
+  alias LiveDebuggerRefactor.App.Utils.Parsers
+  alias LiveDebuggerRefactor.Structs.LvProcess
+
+  @impl true
+  def mount(params, _session, socket) do
+    with {:ok, pid} <- Parsers.string_to_pid(params["pid"]),
+         %LvProcess{} = lv_process <- LiveDebuggerRefactor.Structs.LvProcess.new(pid) do
+      {:ok, assign(socket, :lv_process, lv_process)}
+    else
+      _ ->
+        {:ok, socket}
+    end
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, assign(socket, :params, params)}
+  end
+
+  @impl true
   def render(assigns) do
-    ~H""
+    ~H"""
+    <div class="flex flex-col w-full h-full">
+      <LiveDebuggerRefactor.App.Debugger.CallbackTracing.Web.NodeTracesLive.live_render
+        :if={@lv_process}
+        socket={@socket}
+        id="node-traces"
+        lv_process={@lv_process}
+        params={%{}}
+        class="flex-1"
+      />
+
+      <LiveDebuggerRefactor.App.Debugger.CallbackTracing.Web.GlobalTracesLive.live_render
+        :if={@lv_process}
+        socket={@socket}
+        id="global-traces"
+        lv_process={@lv_process}
+        params={%{}}
+        class="flex-1"
+      />
+      <button phx-click={Phoenix.LiveView.JS.push("open-sidebar", target: "#global-traces")}>
+        Open global traces
+      </button>
+    </div>
+    """
   end
 end
