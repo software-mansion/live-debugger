937e2855e7601b329e4cb6e820f67814025c6014
kraleppa <krzysztof.nalepa@swmansion.com>
2025-08-06 10:54:13 +0200

    Changed FindSuccessor event

--- lib/live_debugger_refactor/app/events.ex
+++ lib/live_debugger_refactor/app/events.ex
@@ -5,6 +5,8 @@ defmodule LiveDebuggerRefactor.App.Events do
 
   use LiveDebuggerRefactor.Event
 
+  alias LiveDebuggerRefactor.Structs.LvProcess
+
   defevent(UserChangedSettings,
     key: :dead_view_mode | :tracing_update_on_code_reload,
     value: term(),
@@ -16,5 +18,5 @@ defmodule LiveDebuggerRefactor.App.Events do
   defevent(DebuggerMounted, debugged_pid: pid(), debugger_pid: pid())
   defevent(DebuggerTerminated, debugged_pid: pid(), debugger_pid: pid())
   defevent(ParamsChanged, params: map())
-  defevent(FindSuccessor, debugged_socket_id: String.t())
+  defevent(FindSuccessor, lv_process: LvProcess.t())
 end
