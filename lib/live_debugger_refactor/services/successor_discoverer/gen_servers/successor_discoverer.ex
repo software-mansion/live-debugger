71c05d718980e623900209505483e03f8d5aa7dc
kraleppa <krzysztof.nalepa@swmansion.com>
2025-08-07 12:20:57 +0200

    Better docs for successor_discoverer

--- lib/live_debugger_refactor/services/successor_discoverer/gen_servers/successor_discoverer.ex
+++ lib/live_debugger_refactor/services/successor_discoverer/gen_servers/successor_discoverer.ex
@@ -1,6 +1,27 @@
 defmodule LiveDebuggerRefactor.Services.SuccessorDiscoverer.GenServers.SuccessorDiscoverer do
   @moduledoc """
-  It receives events from client associated with initializing windows.
+  It discovers successors for debugged LiveView processes.
+  There are two ways to discover successors:
+  1. Using transport pid of the debugged process.
+  2. Using window id of the debugged process.
+
+  Let's say that someone is debugging with LiveDebugger LiveView process in `window_1`
+  It means that there is a WebSocket connection between server and browser that is handled by specific process.
+  The pid of this process is a transport_pid that is stored in the socket of debugged LiveView process.
+  When user is using LiveNavigation to change navigate to different LiveView process WebSocket does not change.
+  That means that transport_pid is the same for both LiveView processes.
+  Knowing that we can find successor by transport_pid.
+
+  The problem occurs when the same user decides to reload the page, or kill the BEAM system process.
+  In such case WebSocket connection is closed, which means that transport_pid will change when new connection is established.
+  This behavior leads to the second approach to discover successors - using window id.
+
+  LiveDebugger requires user to attach our custom JS script to root layout of their debugged application.
+  This script starts another WebSocket connection used for debugging.
+  When this connection is established for the first time we save in sessionStorage window_id (which is a random UUID).
+  Because this value is saved in sessionStorage it will be the same even when you kill the BEAM system process.
+  Knowing that each time the WebSocket connection is established we send to this GenServer window_id and socket_id of LiveView displayed in the window.
+  This way we can find in which window given LiveView is/was displayed, and find its successor.
   """
 
   use GenServer
