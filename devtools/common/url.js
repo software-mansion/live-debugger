9cc02255bf9cfa2c384c5e99a46d4b32b3d4fecc
Alan Guzek <alan.guzek@swmansion.com>
2025-08-20 14:18:25 +0200

    cleaner js

--- devtools/common/url.js
+++ devtools/common/url.js
@@ -1,100 +1,85 @@
 async function getLiveDebuggerSessionURL(browserElement) {
-  return new Promise((resolve, reject) => {
-    const script = `
+  const script = `
       (function() {
-        try {
-          const metaTag = document.querySelector('meta[name="live-debugger-config"]');
-          if (!metaTag) {
-            throw new Error("LiveDebugger meta tag not found!");
-          }
+        const metaTag = document.querySelector('meta[name="live-debugger-config"]');
+        if (!metaTag) {
+          throw new Error("LiveDebugger meta tag not found!");
+        }
 
-          let sessionId = null;
+        function getSessionId() {
           let el;
           if ((el = document.querySelector('[data-phx-main]'))) {
-            sessionId = el.id;
-          } else if ((el = document.querySelector('[id^="phx-"]'))) {
-            sessionId = el.id;
-          } else if ((el = document.querySelector('[data-phx-root-id]'))) {
-            sessionId = el.getAttribute('data-phx-root-id');
+            return el.id;
           }
-
-          return {
-            url: metaTag.getAttribute('url'),
-            version: metaTag.getAttribute('version'),
-            sessionId: sessionId
-          };
-        } catch (error) {
-          throw error;
+          if ((el = document.querySelector('[id^="phx-"]'))) {
+            return el.id;
+          }
+          if ((el = document.querySelector('[data-phx-root-id]'))) {
+            return el.getAttribute('data-phx-root-id');
+          }
+          return null;
         }
+
+        return {
+          url: metaTag.getAttribute('url'),
+          version: metaTag.getAttribute('version'),
+          sessionId: getSessionId()
+        };
       })();
     `;
 
-    browserElement.devtools.inspectedWindow.eval(
-      script,
-      (result, isException) => {
-        if (isException || !result) {
-          reject(new Error("Error fetching LiveDebugger session URL"));
-          return;
-        }
-
-        try {
-          const version = _getVersion(result);
-          const url = _getSessionURL(result.url, version, result.sessionId);
-          resolve(url);
-        } catch (error) {
-          reject(error);
-        }
-      }
+  try {
+    const { url, version, sessionId } = await _runClientScript(
+      browserElement,
+      script
     );
-  });
+
+    return _getSessionURL(url, _getVersion(version), sessionId);
+  } catch (error) {
+    return null;
+  }
 }
 
 async function allowRedirects(browserElement) {
-  return new Promise((resolve, reject) => {
-    const script = `
+  const script = `
       (function() {
         const metaTag = document.querySelector('meta[name="live-debugger-config"]');
         if (metaTag) {
-          return {
-            version: metaTag.getAttribute('version')
-          };
+          return metaTag.getAttribute('version');
         }
         return null;
       })();
     `;
 
+  try {
+    const version = await _runClientScript(browserElement, script);
+    return _isVersionLessThan(_getVersion(version), "0.4");
+  } catch (error) {
+    return false;
+  }
+}
+
+async function _runClientScript(browserElement, script) {
+  return new Promise((resolve, reject) => {
     browserElement.devtools.inspectedWindow.eval(
       script,
       (result, isException) => {
         if (isException) {
-          reject(new Error("Error checking allow redirects"));
+          reject(new Error("Error running client script"));
           return;
         }
-
-        if (result) {
-          const version = _getVersion(result);
-          const shouldAllow = _isVersionLessThan(version, "0.4");
-          resolve(shouldAllow);
-          return;
-        }
-
-        resolve(false);
+        resolve(result);
       }
     );
   });
 }
 
-function _getVersion(metaTagData) {
-  return metaTagData.version ? metaTagData.version : "0.2";
+function _getVersion(version) {
+  return version ? version : "0.2";
 }
 
 function _getSessionURL(baseURL, version, sessionId) {
-  let prefix = "";
-  if (version.startsWith("0.2")) {
-    prefix = "transport_pid";
-  } else {
-    prefix = "redirect";
-  }
+  const prefix = version.startsWith("0.2") ? "transport_pid" : "redirect";
   const session_path = sessionId ? `${prefix}/${sessionId}` : "";
   return `${baseURL}/${session_path}`;
 }
