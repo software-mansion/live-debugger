4c4314ff9f9e98f03b1ae3125d7545d9f2526d92
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2025-08-20 16:45:05 +0200

    Remove old highlighting

--- assets/app/utils/dom.js
+++ assets/app/utils/dom.js
@@ -2,7 +2,8 @@ export function highlightSearchRanges(ranges) {
   let highlight = CSS.highlights.get('search-highlight');
 
   if (highlight) {
-    ranges = [...ranges, ...highlight.values()];
+    old_valid_ranges = highlight.values().filter(({ collapsed }) => !collapsed);
+    ranges = [...ranges, ...old_valid_ranges];
   }
 
   highlight = new Highlight(...ranges);
