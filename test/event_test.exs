0f3948a7a166da7b9f9cd86166f1a04c1491e82d
kraleppa <krzysztof.nalepa@swmansion.com>
2025-08-22 12:30:33 +0200

    Removed event context

--- test/event_test.exs
+++ test/event_test.exs
@@ -10,26 +10,11 @@ defmodule LiveDebugger.EventTest do
     defevent(UserCreated, name: String.t(), email: String.t(), age: integer())
   end
 
-  test "generates a struct with the correct fields with default context" do
+  test "generates a struct with the correct fields" do
     event = %TestEvents.UserCreated{name: "John", email: "john@example.com", age: 30}
 
     assert event.name == "John"
     assert event.email == "john@example.com"
     assert event.age == 30
-    assert event.context == %{}
-  end
-
-  test "generates a struct with the correct fields with custom context" do
-    event = %TestEvents.UserCreated{
-      name: "John",
-      email: "john@example.com",
-      age: 30,
-      context: %{pid: self()}
-    }
-
-    assert event.name == "John"
-    assert event.email == "john@example.com"
-    assert event.age == 30
-    assert event.context == %{pid: self()}
   end
 end
