18458693d9811a27ee6ecc146babd35386527626
Alan Guzek <alan.guzek@swmansion.com>
2025-08-24 17:50:15 +0200

    add dead_view_mode e2e test

---
+++ test/e2e/dead_view_mode_test.exs
@@ -0,0 +1,43 @@
+defmodule LiveDebugger.E2E.DeadViewModeTest do
+  use LiveDebugger.E2ECase
+
+  @sessions 2
+  feature "dead view mode with navigation and disabled highlighting", %{
+    sessions: [dev_app, debugger]
+  } do
+    LiveDebugger.Services.CallbackTracer.GenServers.TracingManager.ping!()
+
+    LiveDebugger.API.SettingsStorage.save(:dead_view_mode, true)
+
+    dev_app
+    |> visit(@dev_app_url)
+
+    debugger
+    |> visit("/")
+    |> click(first_link())
+    |> find(css("#navbar-connected"))
+    |> assert_text("Monitored PID")
+
+    dev_app
+    |> execute_script("window.location.reload();")
+
+    Process.sleep(200)
+
+    debugger
+    |> find(css("#navbar-connected"))
+    |> assert_text("Disconnected")
+
+    debugger
+    |> assert_has(css("#components-tree"))
+    |> click(button("Crash (3)"))
+
+    Process.sleep(200)
+
+    debugger
+    |> find(sidebar_basic_info())
+    |> assert_text("LiveDebuggerDev.LiveComponents.Crash")
+
+    debugger
+    |> assert_has(css("label.pointer-events-none", text: "Highlight"))
+  end
+end
