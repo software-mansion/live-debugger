ff1306d81e8b2f527d1603ccfb06316490251315
Krzysztof Nalepa <56135216+kraleppa@users.noreply.github.com>
2025-08-25 12:21:59 +0200

    Bug: disabling menu when inspect mode changed (#706)

--- assets/client/components/debug_menu.js
+++ assets/client/components/debug_menu.js
@@ -2,9 +2,9 @@ import initDebugButton from './debug_button/debug_button';
 import initDebugOptions from './debug_options/debug_options';
 import { dispatchCustomEvent } from '../utils/dom';
 
-export default function initDebugMenu(liveDebuggerURL) {
+export default function initDebugMenu(liveDebuggerURL, debugChannel) {
   const debugButton = initDebugButton();
-  const debugMenu = initDebugOptions({ liveDebuggerURL });
+  const debugMenu = initDebugOptions({ liveDebuggerURL, debugChannel });
 
   document.body.appendChild(debugButton);
   document.body.appendChild(debugMenu);
