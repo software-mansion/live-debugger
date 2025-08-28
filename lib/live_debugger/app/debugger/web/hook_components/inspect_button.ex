e4bc2258f185910b31e09b28ee27d57eee9d09e3
Alan Guzek <alan.guzek@swmansion.com>
2025-08-28 05:50:27 +0000

    Enhancement: Disable inspecting in dead view mode (#707)
    * disable inspecting in dead view mode
    
    * expand dev example
    
    * clear inspecting when process dies

--- lib/live_debugger/app/debugger/web/hook_components/inspect_button.ex
+++ lib/live_debugger/app/debugger/web/hook_components/inspect_button.ex
@@ -6,7 +6,9 @@ defmodule LiveDebugger.App.Debugger.Web.HookComponents.InspectButton do
 
   use LiveDebugger.App.Web, :hook_component
 
+  alias LiveDebugger.App.Debugger.Events.DeadViewModeEntered
   alias LiveDebugger.Client
+  alias LiveDebugger.Structs.LvProcess
 
   @impl true
   def init(socket) do
@@ -21,14 +23,33 @@ defmodule LiveDebugger.App.Debugger.Web.HookComponents.InspectButton do
   end
 
   attr(:inspect_mode?, :boolean, default: false)
+  attr(:lv_process, LvProcess, required: true)
 
   @impl true
   def render(assigns) do
+    assigns = assign(assigns, :disabled?, !assigns.lv_process.alive?)
+
     ~H"""
     <.tooltip id="inspect-button-tooltip" position="bottom" content="Inspect element on the page">
-      <.nav_icon icon="icon-inspect" selected?={@inspect_mode?} phx-click="switch-inspect-mode" />
+      <.nav_icon
+        icon="icon-inspect"
+        selected?={@inspect_mode?}
+        phx-click="switch-inspect-mode"
+        disabled?={@disabled?}
+      />
     </.tooltip>
     """
+  end
+
+  defp handle_info(%DeadViewModeEntered{debugger_pid: pid}, socket) when pid == self() do
+    Client.push_event!(socket.assigns.root_socket_id, "inspect-mode-changed", %{
+      inspect_mode: false,
+      pid: inspect(self())
+    })
+
+    socket
+    |> assign(:inspect_mode?, false)
+    |> halt()
   end
 
   defp handle_info({"element-inspected", %{"pid" => pid, "url" => url}}, socket) do
