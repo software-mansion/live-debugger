f8ba4cc020063e993ae86905c9e308d78375a977
Alan Guzek <alan.guzek@swmansion.com>
2025-09-04 12:32:28 +0200

    cache details for better performance

--- assets/app/hooks/collapsible.js
+++ assets/app/hooks/collapsible.js
@@ -6,13 +6,16 @@ function setOpen(el) {
   }
 }
 
-function handleCollapsibleEvent(payload, el) {
+function handleCollapsibleEvent(payload, el, cache) {
   if (payload.action === 'toggle') {
     el.open = !el.open;
   } else if (payload.action === 'open') {
     el.open = true;
+    addChildren(el, cache);
   } else if (payload.action === 'close') {
     el.open = false;
+    cache = getChildrenWithoutSummary(el);
+    removeChildren(el, cache);
   } else {
     console.error(
       `Unknown action "${payload.action}" for collapsible with id "${el.id}"`
@@ -20,13 +23,50 @@ function handleCollapsibleEvent(payload, el) {
   }
 }
 
+function getChildrenWithoutSummary(el) {
+  return [...el.childNodes].filter((child) => child.tagName !== 'SUMMARY');
+}
+
+function removeChildren(el, children) {
+  for (child of children) {
+    el.removeChild(child);
+  }
+}
+
+function addChildren(el, children) {
+  for (child of children) {
+    el.appendChild(child);
+  }
+}
+
 const Collapsible = {
   mounted() {
+    let cachedChildNodes = getChildrenWithoutSummary(this.el);
+
+    handleCacheToggle = ({ target, newState }) => {
+      if (newState === 'closed') {
+        cachedChildNodes = getChildrenWithoutSummary(target);
+        removeChildren(target, cachedChildNodes);
+      }
+      if (newState === 'open') {
+        addChildren(target, cachedChildNodes);
+      }
+    };
+
     setOpen(this.el);
 
+    if (this.el.closed) {
+      removeChildren(this.el, cachedChildNodes);
+    }
+
+    this.el.addEventListener('toggle', handleCacheToggle);
+
     this.handleEvent(`${this.el.id}-collapsible`, (payload) => {
-      handleCollapsibleEvent(payload, this.el);
+      handleCollapsibleEvent(payload, this.el, cachedChildNodes);
     });
+  },
+  destroyed() {
+    this.el.removeEventListener('toggle', handleCacheToggle);
   },
 };
 
