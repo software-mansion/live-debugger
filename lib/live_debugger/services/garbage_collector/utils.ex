8741ce8c8e89759d7bcc2f2cc9cac468caaa5721
srzeszut <szczepan.rzeszutek@swmansion.com>
2025-09-11 12:09:09 +0200

    remove approx table size config

--- lib/live_debugger/services/garbage_collector/utils.ex
+++ lib/live_debugger/services/garbage_collector/utils.ex
@@ -4,9 +4,11 @@ defmodule LiveDebugger.Services.GarbageCollector.Utils do
   """
 
   @megabyte_unit 1_048_576
+  @default_table_size 20
 
   # Ets tables might exceed the maximum size since these are approximate values (e.g. 10MB might have 20MB of data).
   @spec max_table_size(:watched | :non_watched) :: non_neg_integer()
+
   def max_table_size(type) when type in [:watched, :non_watched] do
     multiplier =
       case type do
@@ -14,7 +16,7 @@ defmodule LiveDebugger.Services.GarbageCollector.Utils do
         :non_watched -> 0.1
       end
 
-    Application.get_env(:live_debugger, :approx_table_max_size, 10)
+    @default_table_size
     |> Kernel.*(@megabyte_unit)
     |> Kernel.*(multiplier)
     |> round()
