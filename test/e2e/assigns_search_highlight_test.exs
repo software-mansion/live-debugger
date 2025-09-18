e50642bcd7631fa4962692c9c244f3815fa8944c
srzeszut <szczepan.rzeszutek@swmansion.com>
2025-09-18 13:33:33 +0200

    Add assigns search test

---
+++ test/e2e/assigns_search_highlight_test.exs
@@ -0,0 +1,27 @@
+defmodule LiveDebugger.E2E.AssignsSearchHighlightTest do
+  use LiveDebugger.E2ECase
+
+  @sessions 2
+  feature "user can search assigns using the searchbar", %{
+    sessions: [dev_app, debugger]
+  } do
+    dev_app
+    |> visit(@dev_app_url)
+
+    Process.sleep(200)
+
+    debugger
+    |> visit("/")
+    |> click(first_link())
+    |> assert_has(assigns_entry(key: "counter", value: "0"))
+    |> fill_in(assigns_search_bar(), with: "deep value")
+    |> take_screenshot()
+    |> assert_has(css("pre", text: "\"deep value\"", count: 1, visible: true))
+  end
+
+  defp assigns_entry(key: key, value: value) do
+    xpath(
+      ".//*[@id=\"assigns\"]//*[contains(normalize-space(text()), \"#{key}:\")]/../..//*[contains(normalize-space(text()), \"#{value}\")]"
+    )
+  end
+end
