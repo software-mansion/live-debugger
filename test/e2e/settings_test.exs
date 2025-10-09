12f5a6e536b141259c7b7b5f96b34688d22f6962
srzeszut <szczepan.rzeszutek@swmansion.com>
2025-10-09 11:07:19 +0200

    Revert "Bug: Fix appearance buttons in settings are not synchronized across tabs (#736)"
    This reverts commit 6d7c5ce014dd2c0704834a4bffd2abf0ec80ce03.

--- test/e2e/settings_test.exs
+++ test/e2e/settings_test.exs
@@ -1,11 +1,9 @@
 defmodule LiveDebugger.E2E.SettingsTest do
   use LiveDebugger.E2ECase
 
-  @sessions 3
-  @table_name :lvdbg_settings
-
+  @sessions 2
   feature "all settings are working properly", %{
-    sessions: [dev_app, debugger1, debugger2]
+    sessions: [dev_app, debugger]
   } do
     LiveDebugger.Services.CallbackTracer.GenServers.TracingManager.ping!()
 
@@ -17,7 +15,7 @@ defmodule LiveDebugger.E2E.SettingsTest do
 
     # Check dead view mode toggle
 
-    debugger1
+    debugger
     |> visit("/")
     |> click(first_link())
     |> assert_text("Monitored PID")
@@ -27,30 +25,19 @@ defmodule LiveDebugger.E2E.SettingsTest do
 
     Process.sleep(200)
 
-    debugger1
+    debugger
     |> assert_text("Monitored PID")
 
     dev_app
     |> visit(@dev_app_url)
 
-    debugger2
-    |> visit("/settings")
-    |> assert_has(enable_dead_view_mode_checkbox(selected: false))
-    |> assert_has(enable_tracing_update_on_reload_checkbox(selected: false))
-    |> assert_has(enable_garbage_collector_checkbox(selected: false))
-
-    debugger1
+    debugger
     |> visit("/settings")
     |> assert_has(enable_dead_view_mode_checkbox(selected: false))
     |> click(enable_dead_view_mode_toggle())
     |> assert_has(enable_dead_view_mode_checkbox(selected: true))
 
-    debugger2
-    |> assert_has(enable_dead_view_mode_checkbox(selected: true))
-
-    assert(check_dets_for_setting(:dead_view_mode))
-
-    debugger1
+    debugger
     |> visit("/")
     |> click(first_link())
     |> assert_text("Monitored PID")
@@ -60,40 +47,17 @@ defmodule LiveDebugger.E2E.SettingsTest do
 
     Process.sleep(200)
 
-    debugger1
+    debugger
     |> find(css("#navbar-connected"))
     |> assert_text("Disconnected")
 
     # Check tracing update on reload toggle
 
-    debugger1
+    debugger
     |> visit("/settings")
     |> assert_has(enable_tracing_update_on_reload_checkbox(selected: false))
     |> click(enable_tracing_update_on_reload_toggle())
     |> assert_has(enable_tracing_update_on_reload_checkbox(selected: true))
-
-    assert(check_dets_for_setting(:tracing_update_on_code_reload))
-
-    debugger2
-    |> assert_has(enable_tracing_update_on_reload_checkbox(selected: true))
-
-    debugger1
-    |> visit("/settings")
-    |> assert_has(enable_garbage_collector_checkbox(selected: false))
-    |> click(enable_garbage_collector_toggle())
-    |> assert_has(enable_garbage_collector_checkbox(selected: true))
-
-    assert(check_dets_for_setting(:garbage_collection))
-
-    debugger2
-    |> assert_has(enable_garbage_collector_checkbox(selected: true))
-  end
-
-  defp check_dets_for_setting(setting) do
-    case :dets.lookup(@table_name, setting) do
-      [{^setting, value}] -> value
-      _ -> nil
-    end
   end
 
   defp enable_dead_view_mode_toggle() do
