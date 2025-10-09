1559653733280cd2cfc039231b15f715d1ae0a05
Alan Guzek <alan.guzek@swmansion.com>
2025-10-09 13:22:17 +0200

    adjust TermNode

--- lib/live_debugger/app/utils/term_parser.ex
+++ lib/live_debugger/app/utils/term_parser.ex
@@ -18,25 +18,34 @@ defmodule LiveDebugger.App.Utils.TermParser do
     @moduledoc """
     Represents a node in the display tree.
 
-    - `id`: The id of the node (it represents the path to the node in the tree).
+    - `id`: The id of the node. It uses dot notation to represent the path to the node.
+    - `open?`: Whether the node is expanded
     - `kind`: The type of the node (e.g., :atom, :list, :map).
-    - `children`: A list of child nodes.
+    - `children`: A map of child nodes with keys (integer indices for lists/tuples, actual keys for maps).
     - `content`: Display elements that represent the content of the node when has no children or not expanded.
     - `expanded_before`: Display elements shown before the node's children when expanded.
     - `expanded_after`: Display elements shown after the node's children when expanded.
     """
-    defstruct [:id, :kind, :children, :content, :expanded_before, :expanded_after]
+    defstruct [:id, :kind, :children, :content, :expanded_before, :expanded_after, open?: false]
 
     @type kind() :: :atom | :binary | :number | :tuple | :list | :map | :struct | :regex | :other
 
     @type t :: %__MODULE__{
             id: String.t(),
             kind: kind(),
-            children: [t()],
+            open?: boolean(),
+            children: [{any(), t()}],
             content: [DisplayElement.t()],
             expanded_before: [DisplayElement.t()] | nil,
             expanded_after: [DisplayElement.t()] | nil
           }
+
+    @spec has_children?(t()) :: boolean()
+    def has_children?(%__MODULE__{children: []}), do: false
+    def has_children?(%__MODULE__{}), do: true
+
+    @spec children_number(t()) :: integer()
+    def children_number(%__MODULE__{children: children}), do: length(children)
   end
 
   @spec term_to_copy_string(term()) :: String.t()
@@ -53,6 +62,36 @@ defmodule LiveDebugger.App.Utils.TermParser do
   @spec term_to_display_tree(term()) :: TermNode.t()
   def term_to_display_tree(term) do
     to_node(term, [], "root")
+  end
+
+  @spec update_by_id(term(), String.t(), (TermNode.t() -> {:ok, TermNode.t()} | {:error, any()})) ::
+          {:ok, term()} | {:error, any()}
+  def update_by_id(term, "root", update_fn) do
+    update_fn.(term)
+  end
+
+  def update_by_id(term, "root" <> _ = id, update_fn) do
+    ["root" | string_path] = String.split(id, ".")
+    path = string_path |> Enum.map(&String.to_integer/1) |> IO.inspect(label: "path")
+    update_by_path(term, path, update_fn)
+  end
+
+  defp update_by_path(term, [index | path], update_fn) do
+    with {key, child} <- Enum.at(term.children, index),
+         {:ok, updated_child} <- update_by_path(child, path, update_fn) do
+      children = List.replace_at(term.children, index, {key, updated_child})
+      {:ok, %TermNode{term | children: children}}
+    else
+      nil ->
+        {:error, :child_not_found}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp update_by_path(term, [], update_fn) do
+    update_fn.(term)
   end
 
   @spec to_node(term(), [DisplayElement.t()], String.t()) :: TermNode.t()
@@ -121,7 +160,7 @@ defmodule LiveDebugger.App.Utils.TermParser do
 
     map = Map.from_struct(struct)
     size = map_size(map)
-    children = to_key_value_children(map, size, id_path)
+    children = map |> Map.to_list() |> to_key_value_children(size, id_path)
 
     branch_node(
       id_path,
@@ -150,6 +189,8 @@ defmodule LiveDebugger.App.Utils.TermParser do
     leaf_node(id_path, :other, [black(inspect(other)) | suffix])
   end
 
+  @spec to_key_value_node({any(), any()}, [DisplayElement.t()], String.t()) ::
+          {any(), TermNode.t()}
   defp to_key_value_node({key, value}, suffix, id_path) do
     {key_span, sep_span} =
       case to_node(key, [], "not_used_id_path") do
@@ -164,25 +205,27 @@ defmodule LiveDebugger.App.Utils.TermParser do
            black(" => ")}
       end
 
-    case to_node(value, suffix, id_path) do
-      %TermNode{content: content, children: []} = node ->
-        %TermNode{node | content: [key_span, sep_span | content]}
+    node = to_node(value, suffix, id_path)
+    node = %TermNode{node | content: [key_span, sep_span | node.content]}
 
-      %TermNode{content: content, expanded_before: expanded_before} = node ->
-        %TermNode{
-          node
-          | content: [key_span, sep_span | content],
-            expanded_before: [key_span, sep_span | expanded_before]
-        }
-    end
+    node =
+      if TermNode.has_children?(node) do
+        %TermNode{node | expanded_before: [key_span, sep_span | node.expanded_before]}
+      else
+        node
+      end
+
+    {key, node}
   end
 
+  @spec to_children(list(), integer(), String.t()) :: [{integer(), TermNode.t()}]
   defp to_children(items, container_size, id_path) do
     Enum.with_index(items, fn item, index ->
-      to_node(item, suffix(index, container_size), "#{id_path}.#{index}")
+      {index, to_node(item, suffix(index, container_size), "#{id_path}.#{index}")}
     end)
   end
 
+  @spec to_key_value_children([{any(), any()}], integer(), String.t()) :: [{any(), TermNode.t()}]
   defp to_key_value_children(items, container_size, id_path) do
     Enum.with_index(items, fn item, index ->
       to_key_value_node(item, suffix(index, container_size), "#{id_path}.#{index}")
