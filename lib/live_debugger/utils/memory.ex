2a96757f27552e7abe96bc9413cd56528b7d3e95
kraleppa <krzysztof.nalepa@swmansion.com>
2025-10-10 09:49:23 +0200

    Added more memory measure functions

--- lib/live_debugger/utils/memory.ex
+++ lib/live_debugger/utils/memory.ex
@@ -4,11 +4,31 @@ defmodule LiveDebugger.Utils.Memory do
   These functions provide only a good approximation
   """
 
+  @wordsize :erlang.system_info(:wordsize)
+
   @doc """
   Returns the approximate size of an elixir term in bytes.
   """
   @spec approx_term_size(term :: term()) :: non_neg_integer()
   def approx_term_size(term) do
     term |> :erlang.term_to_binary() |> byte_size()
+  end
+
+  @doc """
+  Measures term size inside process heap.
+  Ignores large binaries/strings (> 64 bytes).
+  """
+  @spec term_heap_size(term :: term()) :: non_neg_integer()
+  def term_heap_size(term) do
+    :erts_debug.size(term) * @wordsize
+  end
+
+  @doc """
+  Measures the total memory that would be needed if the assigns were deep-copied.
+  Includes large off-heap binaries.
+  """
+  @spec term_total_size(term :: term()) :: non_neg_integer()
+  def term_total_size(term) do
+    :erts_debug.flat_size(term) * @wordsize
   end
 end
