83a098934045e74de25aac6f909381d8ccbb8f88
kraleppa <krzysztof.nalepa@swmansion.com>
2025-10-16 12:07:27 +0200

    Proper diff size calculation

--- lib/live_debugger/services/callback_tracer/actions/diff_trace.ex
+++ lib/live_debugger/services/callback_tracer/actions/diff_trace.ex
@@ -14,12 +14,11 @@ defmodule LiveDebugger.Services.CallbackTracer.Actions.DiffTrace do
   @spec maybe_create_diff(non_neg_integer(), pid(), non_neg_integer(), iodata()) ::
           {:ok, DiffTrace.t()} | {:error, term()}
   def maybe_create_diff(n, pid, timestamp, iodata) do
-    body_binary = IO.iodata_to_binary(iodata)
-    body_size = byte_size(body_binary)
-
-    with {:ok, message_json} <- Jason.decode(body_binary),
-         diff <- get_diff(message_json) do
-      {:ok, DiffTrace.new(n, diff, pid, timestamp, body_size)}
+    with body_binary <- IO.iodata_to_binary(iodata),
+         {:ok, message_json} <- Jason.decode(body_binary),
+         diff <- get_diff(message_json),
+         {:ok, diff_size} <- get_diff_size(diff) do
+      {:ok, DiffTrace.new(n, diff, pid, timestamp, diff_size)}
     end
   end
 
@@ -44,6 +43,13 @@ defmodule LiveDebugger.Services.CallbackTracer.Actions.DiffTrace do
   end
 
   defp get_diff([_, _, _, "diff", payload]), do: payload
-  defp get_diff([_, _, _, _type, %{"response" => %{"diff" => diff}}]), do: diff
+  defp get_diff([_, _, _, _type, %{"response" => response}]), do: response
   defp get_diff(_), do: %{}
+
+  defp get_diff_size(diff) do
+    case Jason.encode(diff) do
+      {:ok, diff_binary} -> {:ok, byte_size(diff_binary)}
+      {:error, _} -> {:error, "Could not encode diff"}
+    end
+  end
 end
