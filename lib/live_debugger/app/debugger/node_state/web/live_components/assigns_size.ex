1c8841421b311a2ec8cc80fb940227b5d668fe01
kraleppa <krzysztof.nalepa@swmansion.com>
2025-10-17 12:11:26 +0200

    Async loading of assigns

--- lib/live_debugger/app/debugger/node_state/web/live_components/assigns_size.ex
+++ lib/live_debugger/app/debugger/node_state/web/live_components/assigns_size.ex
@@ -6,11 +6,14 @@ defmodule LiveDebugger.App.Debugger.NodeState.Web.LiveComponents.AssignsSize do
   use LiveDebugger.App.Web, :live_component
 
   alias LiveDebugger.Utils.Memory
+  alias Phoenix.LiveView.AsyncResult
 
   @impl true
   def update(%{assigns: assigns, id: id}, socket) do
     socket
     |> assign(assigns: assigns)
+    |> assign(:assigns_sizes, AsyncResult.loading())
+    |> start_async(:assigns_size, fn -> calculate_assigns_size(assigns) end)
     |> assign(:id, id)
     |> ok()
   end
@@ -20,25 +23,50 @@ defmodule LiveDebugger.App.Debugger.NodeState.Web.LiveComponents.AssignsSize do
     ~H"""
     <div class="text-xs text-secondary-text flex gap-1">
       <span>Assigns size: </span>
-      <.tooltip
-        id={@id <> "-tooltip-heap"}
-        content="Memory used by assigns inside the LiveView process."
-        class="truncate"
-        position="top-center"
-      >
-        <span><%= assigns_heap_size(assigns) %> heap</span>
-      </.tooltip>
-      <span> / </span>
-      <.tooltip
-        id={@id <> "-tooltip-serialized"}
-        content="Size of assigns when encoded for transfer."
-        class="truncate"
-        position="top-center"
-      >
-        <%= assigns_serialized_size(@assigns) %> serialized
-      </.tooltip>
+      <.async_result :let={assigns_sizes} assign={@assigns_sizes}>
+        <.tooltip
+          id={@id <> "-tooltip-heap"}
+          content="Memory used by assigns inside the LiveView process."
+          class="truncate"
+          position="top-center"
+        >
+          <span><%= assigns_sizes.heap_size %> heap</span>
+        </.tooltip>
+        <span> / </span>
+        <.tooltip
+          id={@id <> "-tooltip-serialized"}
+          content="Size of assigns when encoded for transfer."
+          class="truncate"
+          position="top-center"
+        >
+          <span><%= assigns_sizes.serialized_size %> serialized</span>
+        </.tooltip>
+        <:loading>
+          <.spinner size="sm" />
+        </:loading>
+        <:failed>
+          <span class="text-red-700"> error </span>
+        </:failed>
+      </.async_result>
     </div>
     """
+  end
+
+  @impl true
+  def handle_async(:assigns_size, {:ok, assigns_sizes}, socket) do
+    socket
+    |> assign(:assigns_sizes, AsyncResult.ok(assigns_sizes))
+    |> noreply()
+  end
+
+  def handle_async(:assigns_size, {:exit, {reason, _}}, socket) do
+    socket
+    |> assign(:assigns_sizes, AsyncResult.failed(%AsyncResult{}, reason))
+    |> noreply()
+  end
+
+  defp calculate_assigns_size(assigns) do
+    %{heap_size: assigns_heap_size(assigns), serialized_size: assigns_serialized_size(assigns)}
   end
 
   defp assigns_heap_size(assigns) do
