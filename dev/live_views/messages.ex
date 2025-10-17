aab323d95fe76f1a69533830225272620bd946fb
Alan Guzek <alan.guzek@swmansion.com>
2025-10-17 11:04:32 +0200

    add big messages in assigns

--- dev/live_views/messages.ex
+++ dev/live_views/messages.ex
@@ -1,11 +1,20 @@
 defmodule LiveDebuggerDev.LiveViews.Messages do
   use DevWeb, :live_view
 
+  def mount(_params, _session, socket) do
+    socket
+    |> assign(:messages, [])
+    |> ok()
+  end
+
+  attr(:messages, :list, required: true)
+
   def render(assigns) do
     ~H"""
     <.box title="Messages [LiveView]" color="purple">
       <div>
         <.button phx-click="big-message" color="purple">Send big message</.button>
+        <div>Message count: <%= length(@messages) %></div>
       </div>
     </.box>
     """
@@ -13,7 +22,12 @@ defmodule LiveDebuggerDev.LiveViews.Messages do
 
   def handle_event("big-message", _, socket) do
     send(self(), very_big_message())
-    {:noreply, socket}
+
+    socket
+    |> assign(:messages, [
+      {length(socket.assigns.messages), very_big_message()} | socket.assigns.messages
+    ])
+    |> noreply()
   end
 
   def handle_info(_, socket) do
