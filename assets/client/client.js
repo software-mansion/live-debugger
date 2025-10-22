0267a74cecaf2bab969f814694f31469e710caa6
Hubert Kasprzycki <67634024+hhubert6@users.noreply.github.com>
2025-10-22 09:06:02 +0200

    Bug: Highlighting not working with embedded LiveViews (#797)
    * Fix
    
    * Fix finding root element id when inspecting embedded LiveView
    
    * Send list of all root socket ids via websocket when no main LiveView
    
    * Subscribe client channel to all root socket ids
    
    * Remove alias
    
    * Fix test
    
    * Update assets
    
    * CR suggestions

--- assets/client/client.js
+++ assets/client/client.js
@@ -10,25 +10,35 @@ import initTooltip from './components/tooltip/tooltip';
 import {
   getMetaTag,
   fetchLiveDebuggerBaseURL,
-  fetchDebuggedSocketID,
+  fetchDebuggedSocketIDs,
 } from './utils/meta';
 
-window.document.addEventListener('DOMContentLoaded', function () {
+window.document.addEventListener('DOMContentLoaded', async () => {
   const metaTag = getMetaTag();
   const baseURL = fetchLiveDebuggerBaseURL(metaTag);
-  const socketID = fetchDebuggedSocketID();
+  let { mainSocketID, rootSocketIDs } = await fetchDebuggedSocketIDs();
 
-  const sessionURL = `${baseURL}/redirect/${socketID}`;
+  if (!mainSocketID) {
+    [mainSocketID, ...rootSocketIDs] = rootSocketIDs;
+  } else {
+    rootSocketIDs = [];
+  }
 
-  if (socketID) {
-    const { debugChannel } = initDebugSocket(baseURL, socketID);
+  if (mainSocketID) {
+    const sessionURL = `${baseURL}/redirect/${mainSocketID}`;
+
+    const { debugChannel } = initDebugSocket(
+      baseURL,
+      mainSocketID,
+      rootSocketIDs
+    );
 
     debugChannel.on('ping', (resp) => {
       console.log('Received ping', resp);
       debugChannel.push('pong', resp);
     });
 
-    initElementInspection({ baseURL, debugChannel, socketID });
+    initElementInspection({ baseURL, debugChannel, socketID: mainSocketID });
     initTooltip();
     initDebugMenu(metaTag, sessionURL, debugChannel);
     initHighlight(debugChannel);
