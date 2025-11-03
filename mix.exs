1ece59647000aae532ae2b9ead5937e8478b1438
kraleppa <krzysztof.nalepa@swmansion.com>
2025-11-03 12:33:11 +0100

    Removed listeners from `mix.exs`

--- mix.exs
+++ mix.exs
@@ -4,7 +4,6 @@ defmodule LiveDebugger.MixProject do
   @version "0.5.0-dev"
 
   def project do
-
     [
       app: :live_debugger,
       version: @version,
@@ -20,8 +19,7 @@ defmodule LiveDebugger.MixProject do
       docs: docs(),
       test_coverage: [
         ignore_modules: [~r/^LiveDebuggerDev\./, DevWeb]
-      ],
-      listeners: [Phoenix.CodeReloader]
+      ]
     ]
   end
 
