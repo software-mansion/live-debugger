4745674b2af80fe20276b01eb4596396068b27c9
srzeszut <szczepan.rzeszutek@swmansion.com>
2025-11-04 17:47:38 +0100

    add support for phoenix 1.0.17

--- lib/live_debugger/app/debugger/node_state/stream_utils.ex
+++ lib/live_debugger/app/debugger/node_state/stream_utils.ex
@@ -178,14 +178,25 @@ defmodule LiveDebugger.App.Debugger.NodeState.StreamUtils do
   end
 
   defp create_insert_functions(inserts, name) do
-    Enum.map(inserts, fn {_dom_id, at, element, limit, update?} ->
-      fn socket ->
-        Phoenix.LiveView.stream_insert(socket, name, element,
-          at: at,
-          limit: limit,
-          update: update?
-        )
-      end
+    Enum.map(inserts, fn
+      {_dom_id, at, element, limit, update?} ->
+        fn socket ->
+          Phoenix.LiveView.stream_insert(socket, name, element,
+            at: at,
+            limit: limit,
+            update: update?
+          )
+        end
+
+      # https://hexdocs.pm/phoenix_live_view/1.0.17/Phoenix.LiveView.html#stream_insert/4
+      # In phoenix 1.0.17 insert doesn't have update option
+      {_dom_id, at, element, limit} ->
+        fn socket ->
+          Phoenix.LiveView.stream_insert(socket, name, element,
+            at: at,
+            limit: limit
+          )
+        end
     end)
   end
 
