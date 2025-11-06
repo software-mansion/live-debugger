0d765523e31eadaf3566e6cf2f984072df06bdf0
p-malecki <92953623+p-malecki@users.noreply.github.com>
2025-11-06 11:52:19 +0000

    Deploying to gh-pages from  @ 8a984926929689156309f2e80729bc9361a1530f 🚀

--- _astro/MixInstallTab.DWdKVKK3.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./utils.CP-DEBaU.js";import{I as t,C as o}from"./InlineCode.DOUEJKR9.js";import"./index.S7Dpkimb.js";import"./button.Mrt4GEO5.js";import"./index.-KpfnFlO.js";import"./index.CwgFG58H.js";import"./createLucideIcon.rtP_7z7L.js";const r=`defp deps do
-  [
-    {:live_debugger, "~> 0.4.0", only: :dev}
-  ]
-end`,i=`# lib/my_app_web/components/layouts/root.html.heex
-
-<head>
-  <%= Application.get_env(:live_debugger, :live_debugger_tags) %>
-</head>`;function x(){return e.jsxs("div",{className:"flex flex-col gap-6",children:[e.jsxs("p",{className:"text-primary-foreground text-md font-normal",children:["Add ",e.jsx(t,{children:"live_debugger"})," to your list of dependencies in ",e.jsx(t,{children:"mix.exs"})," :"]}),e.jsx(o,{codeString:r}),e.jsx("br",{}),e.jsxs("p",{className:"text-primary-foreground text-md font-normal",children:["Add a line to your application root layout. It attaches"," ",e.jsx(t,{children:"meta"})," tag and LiveDebugger scripts."]}),e.jsx(o,{codeString:i}),e.jsx("br",{}),e.jsxs("p",{className:"text-primary-foreground text-md font-normal",children:["After you start your application, LiveDebugger will be running at a default port ",e.jsx(t,{children:"http://localhost:4007"}),"."]})]})}export{x as default};
