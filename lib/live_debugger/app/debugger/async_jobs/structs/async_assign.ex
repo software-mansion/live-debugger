4de7f59d43c8a0cac94cf8ecd1337402083c0af7
kraleppa <krzysztof.nalepa@swmansion.com>
2025-11-13 13:28:18 +0100

    Added structs

---
+++ lib/live_debugger/app/debugger/async_jobs/structs/async_assign.ex
@@ -0,0 +1,22 @@
+defmodule LiveDebugger.App.Debugger.AsyncJobs.Structs.AsyncAssign do
+  @moduledoc """
+  This module provides a struct to represent an async assign background task.
+  """
+
+  defstruct [:pid, :keys, :ref]
+
+  @type t() :: %__MODULE__{
+          pid: pid(),
+          keys: [atom()],
+          ref: reference()
+        }
+
+  @spec new(pid(), [atom()], reference()) :: t()
+  def new(pid, keys, ref) do
+    %__MODULE__{
+      pid: pid,
+      keys: keys,
+      ref: ref
+    }
+  end
+end
