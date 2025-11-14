2fdd41f0193fc3b554ecc472add8ea18247e2127
kraleppa <krzysztof.nalepa@swmansion.com>
2025-11-14 15:04:11 +0100

    Display async jobs based on trace

--- lib/live_debugger/app/debugger/async_jobs/queries.ex
+++ lib/live_debugger/app/debugger/async_jobs/queries.ex
@@ -5,23 +5,57 @@ defmodule LiveDebugger.App.Debugger.AsyncJobs.Queries do
 
   alias LiveDebugger.API.LiveViewDebug
   alias LiveDebugger.API.StatesStorage
-
+  alias LiveDebugger.API.TracesStorage
   alias LiveDebugger.Structs.LvState
+  alias LiveDebugger.Structs.Trace.FunctionTrace
+  alias Phoenix.LiveView.Socket
   alias LiveDebugger.App.Debugger.AsyncJobs.Structs.AsyncJob
   alias LiveDebugger.App.Debugger.AsyncJobs.Structs.AsyncJob.StartAsync
   alias LiveDebugger.App.Debugger.AsyncJobs.Structs.AsyncJob.AsyncAssign
 
-  @spec fetch_async_jobs(pid()) :: {:ok, [AsyncJob.t()]} | {:error, term()}
-  def fetch_async_jobs(pid) do
+  @spec fetch_async_jobs(pid() | {reference(), integer()}) ::
+          {:ok, [AsyncJob.t()]} | {:error, term()}
+  def fetch_async_jobs(pid) when is_pid(pid) do
     with {:ok, state} <- fetch_node_state(pid),
          live_async <- get_live_async(state) do
-      jobs =
-        live_async
-        |> Enum.map(&parse_job/1)
-        |> Enum.reject(&is_nil/1)
-
-      {:ok, jobs}
+      {:ok, parse_live_async(live_async)}
     end
+  end
+
+  def fetch_async_jobs({ets_ref, trace_id}) do
+    with trace when not is_nil(trace) <- TracesStorage.get_by_id!(ets_ref, trace_id),
+         live_async <- get_live_async(trace) do
+      {:ok, parse_live_async(live_async)}
+    else
+      nil ->
+        {:error, "Trace not found"}
+    end
+  end
+
+  defp fetch_node_state(pid) do
+    case StatesStorage.get!(pid) do
+      nil ->
+        LiveViewDebug.liveview_state(pid)
+
+      state ->
+        {:ok, state}
+    end
+  end
+
+  defp get_live_async(%LvState{socket: %{private: %{live_async: live_async}}}), do: live_async
+
+  defp get_live_async(%FunctionTrace{
+         return_value: {_, %Socket{private: %{live_async: live_async}}}
+       }) do
+    live_async
+  end
+
+  defp get_live_async(_), do: %{}
+
+  defp parse_live_async(live_async) do
+    live_async
+    |> Enum.map(&parse_job/1)
+    |> Enum.reject(&is_nil/1)
   end
 
   defp parse_job({name, {ref, pid, :start}}) when is_atom(name) do
@@ -31,14 +65,4 @@ defmodule LiveDebugger.App.Debugger.AsyncJobs.Queries do
   defp parse_job({keys, {ref, pid, :assign}}) when is_list(keys) do
     AsyncAssign.new(pid, keys, ref)
   end
-
-  defp fetch_node_state(pid) do
-    case StatesStorage.get!(pid) do
-      nil -> LiveViewDebug.liveview_state(pid)
-      state -> {:ok, state}
-    end
-  end
-
-  defp get_live_async(%LvState{socket: %{private: %{live_async: live_async}}}), do: live_async
-  defp get_live_async(_), do: %{}
 end
--- lib/live_debugger/app/debugger/async_jobs/web/async_jobs.ex
+++ lib/live_debugger/app/debugger/async_jobs/web/async_jobs.ex
@@ -5,10 +5,11 @@ defmodule LiveDebugger.App.Debugger.AsyncJobs.Web.AsyncJobsLive do
 
   use LiveDebugger.App.Web, :live_view
 
+  alias Phoenix.LiveView.AsyncResult
   alias LiveDebugger.Structs.LvProcess
   alias LiveDebugger.Bus
   alias LiveDebugger.App.Debugger.Events.NodeIdParamChanged
-  alias LiveDebugger.Services.CallbackTracer.Events.StateChanged
+  alias LiveDebugger.Services.CallbackTracer.Events.TraceReturned
 
   alias LiveDebugger.App.Debugger.AsyncJobs.Queries, as: AsyncJobsQueries
   alias LiveDebugger.App.Debugger.AsyncJobs.Structs.AsyncJob
@@ -54,14 +55,15 @@ defmodule LiveDebugger.App.Debugger.AsyncJobs.Web.AsyncJobsLive do
 
     if connected?(socket) do
       Bus.receive_events!(parent_pid)
-      Bus.receive_states!(lv_process.pid)
+      Bus.receive_traces!(lv_process.pid)
     end
 
     socket
     |> assign(:lv_process, lv_process)
     |> assign(:node_id, node_id)
     |> assign(:assigns_search_phrase, "")
-    |> assign(:async_jobs, [])
+    |> assign(:async_jobs, AsyncResult.loading())
+    |> start_async(:fetch_async_jobs, fn -> AsyncJobsQueries.fetch_async_jobs(lv_process.pid) end)
     |> ok()
   end
 
@@ -71,14 +73,28 @@ defmodule LiveDebugger.App.Debugger.AsyncJobs.Web.AsyncJobsLive do
     <div class="max-w-full flex flex-1">
       <.section title="Async jobs" id="async-jobs" inner_class="mx-0 my-4 px-4" class="flex-1">
         <div class="w-full h-full flex flex-col gap-4">
-          <%= for async_job <- @async_jobs do %>
-            <div class="flex gap-2">
-              <span class="text-sm">
-                <%= inspect(AsyncJob.identifier(async_job)) %>
-              </span>
-              <span class="text-sm"><%= inspect(async_job.pid) %></span>
-            </div>
-          <% end %>
+          <.async_result :let={async_jobs} assign={@async_jobs}>
+            <:loading>
+              <div class="flex justify-center items-center h-full">
+                <.spinner size="sm" />
+              </div>
+            </:loading>
+            <:failed>
+              <div class="flex justify-center items-center h-full">
+                <.alert class="w-full" with_icon heading="Error while fetching async jobs">
+                  Check logs for more
+                </.alert>
+              </div>
+            </:failed>
+            <%= for async_job <- async_jobs do %>
+              <div class="flex gap-2">
+                <span class="text-sm">
+                  <%= inspect(AsyncJob.identifier(async_job)) %>
+                </span>
+                <span class="text-sm"><%= inspect(async_job.pid) %></span>
+              </div>
+            <% end %>
+          </.async_result>
         </div>
       </.section>
     </div>
@@ -92,15 +108,41 @@ defmodule LiveDebugger.App.Debugger.AsyncJobs.Web.AsyncJobsLive do
     |> noreply()
   end
 
-  def handle_info(%StateChanged{}, socket) do
-    dbg("State changed")
-
-    {:ok, async_jobs} = AsyncJobsQueries.fetch_async_jobs(socket.assigns.lv_process.pid)
+  def handle_info(%TraceReturned{function: :render}, socket) do
+    pid = socket.assigns.lv_process.pid
 
     socket
-    |> assign(:async_jobs, async_jobs)
+    |> start_async(:fetch_async_jobs, fn -> AsyncJobsQueries.fetch_async_jobs(pid) end)
+    |> noreply()
+  end
+
+  def handle_info(%TraceReturned{ets_ref: ets_ref, trace_id: trace_id}, socket) do
+    trace_tuple = {ets_ref, trace_id}
+
+    socket
+    |> start_async(:fetch_async_jobs, fn -> AsyncJobsQueries.fetch_async_jobs(trace_tuple) end)
     |> noreply()
   end
 
   def handle_info(_, socket), do: {:noreply, socket}
+
+  @impl true
+  def handle_async(:fetch_async_jobs, {:ok, {:ok, async_jobs}}, socket)
+      when is_list(async_jobs) do
+    socket
+    |> assign(:async_jobs, AsyncResult.ok(async_jobs))
+    |> noreply()
+  end
+
+  def handle_async(:fetch_async_jobs, {:ok, {:error, reason}}, socket) do
+    socket
+    |> assign(:async_jobs, AsyncResult.failed(AsyncResult.loading(), reason))
+    |> noreply()
+  end
+
+  def handle_async(:fetch_async_jobs, {:exit, reason}, socket) do
+    socket
+    |> assign(:async_jobs, AsyncResult.failed(AsyncResult.loading(), reason))
+    |> noreply()
+  end
 end
