4e730f7caf8b65bdd3a761bac1350c93ca681a7d
kraleppa <krzysztof.nalepa@swmansion.com>
2025-12-11 13:28:23 +0100

    Added support for handle_event

--- lib/live_debugger/api/user_events.ex
+++ lib/live_debugger/api/user_events.ex
@@ -3,23 +3,53 @@ defmodule LiveDebugger.API.UserEvents do
   API for user events.
   """
 
-  defguard is_cid(cid) when is_struct(cid, Phoenix.LiveComponent.CID)
+  alias LiveDebugger.Structs.LvProcess
+  alias Phoenix.LiveComponent.CID
+  alias Phoenix.Socket.Message
 
-  def send_event(pid, cid \\ nil, handler_type, payload)
+  def send_event(lv_process, cid \\ nil, handler_type, payload)
 
-  def send_event(pid, cid, :update, payload) when is_pid(pid) and is_cid(cid) do
-    Phoenix.LiveView.send_update(pid, cid, payload)
+  def send_event(%LvProcess{} = lv_process, %CID{} = cid, :update, payload) do
+    Phoenix.LiveView.send_update(lv_process.pid, cid, payload)
   end
 
-  def send_event(pid, nil, :handle_info, payload) when is_pid(pid) do
-    send(pid, payload)
+  def send_event(%LvProcess{} = lv_process, nil, :handle_info, payload) do
+    send(lv_process.pid, payload)
   end
 
-  def send_event(pid, nil, :handle_cast, payload) when is_pid(pid) do
-    GenServer.cast(pid, payload)
+  def send_event(%LvProcess{} = lv_process, nil, :handle_cast, payload) do
+    GenServer.cast(lv_process.pid, payload)
   end
 
-  def send_event(pid, nil, :handle_call, payload) when is_pid(pid) do
-    GenServer.call(pid, payload)
+  def send_event(%LvProcess{} = lv_process, nil, :handle_call, payload) do
+    GenServer.call(lv_process.pid, payload)
+  end
+
+  def send_event(%LvProcess{} = lv_process, nil, :handle_event, event, params) do
+    payload = %{"event" => event, "value" => params, "type" => "debug"}
+
+    message = %Message{
+      topic: "lv:#{lv_process.socket_id}",
+      event: "event",
+      payload: payload
+    }
+
+    send(lv_process.pid, message)
+  end
+
+  def send_event(%LvProcess{} = lv_process, %CID{cid: cid}, :handle_event, event, params) do
+    payload = %{"cid" => cid, "event" => event, "value" => params, "type" => "debug"}
+
+    message = %Message{
+      topic: "lv:#{lv_process.socket_id}",
+      event: "event",
+      payload: payload
+    }
+
+    send(lv_process.pid, message)
   end
 end
+
+# alias LiveDebugger.API.UserEvents
+# [lv_process] = LiveDebugger.API.LiveViewDiscovery.debugged_lv_processes
+# UserEvents.send_event(lv_process, %Phoenix.LiveComponent.CID{cid: 6}, :handle_event, "show_child", %{})
