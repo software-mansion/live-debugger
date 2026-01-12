5fe68e0e7e3d7aa23d363065fba7ecb7738d91d5
kraleppa <krzysztof.nalepa@swmansion.com>
2026-01-12 13:06:00 +0100

    Removed JS logs

--- config/config.exs
+++ config/config.exs
@@ -78,6 +78,7 @@ if config_env() == :test do
     driver: Wallaby.Chrome,
     otp_app: :live_debugger,
     chrome: [headless: true],
+    js_logger: nil,
     screenshot_on_failure: true,
     screenshot_dir: "tmp/screenshots"
 
