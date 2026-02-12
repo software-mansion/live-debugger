1ffe39a26232b08ec5e01fe8df0fbca71666b0fb
srzeszut <szczepan.rzeszutek@swmansion.com>
2026-02-12 15:19:34 +0100

    change detecting editor logic

---
+++ lib/live_debugger/app/debugger/web/utils/editor.ex
@@ -0,0 +1,57 @@
+defmodule LiveDebugger.App.Debugger.Web.Utils.Editor do
+  @moduledoc """
+  Utilities for opening editors
+  """
+
+  @term_to_cmd %{
+    "vscode" => "code -g",
+    "zed" => "zed"
+  }
+
+  require Logger
+
+  @spec detect_editor() :: String.t() | nil
+  def detect_editor() do
+    cond do
+      elixir_editor = System.get_env("ELIXIR_EDITOR") ->
+        elixir_editor
+
+      system_editor = System.get_env("EDITOR") ->
+        system_editor
+
+      mapped_editor = Map.get(@term_to_cmd, System.get_env("TERM_PROGRAM")) ->
+        mapped_editor
+
+      true ->
+        nil
+    end
+  end
+
+  @spec get_editor_cmd(String.t(), String.t(), integer()) :: String.t()
+  def get_editor_cmd(editor, file, line)
+      when is_binary(file) and is_integer(line) and is_binary(editor) do
+    if editor =~ "__FILE__" or editor =~ "__LINE__" do
+      editor
+      |> String.replace("__FILE__", inspect(file))
+      |> String.replace("__LINE__", Integer.to_string(line))
+    else
+      "#{editor} #{inspect(file)}:#{line}"
+    end
+  end
+
+  @spec run_shell_cmd(String.t()) :: :ok | {:error, term()}
+  def run_shell_cmd(command) do
+    case System.shell(command, stderr_to_stdout: true) do
+      {_output, 0} ->
+        :ok
+
+      {path, 127} ->
+        Logger.error("Editor not found at path: #{path}")
+        {:error, :not_found}
+
+      {output, status} ->
+        Logger.error("Command failed with status #{status}. Output: #{output}")
+        {:error, status}
+    end
+  end
+end
