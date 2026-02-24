34275a9f026bdee202a5764b01a3469da7c71d95
kraleppa <krzysztof.nalepa@swmansion.com>
2026-02-24 13:03:28 +0100

    Improved fetching in active liveviews

--- lib/live_debugger/app/discovery/queries.ex
+++ lib/live_debugger/app/discovery/queries.ex
@@ -14,18 +14,58 @@ defmodule LiveDebugger.App.Discovery.Queries do
   @doc """
   Fetches all active LiveView processes grouped by their transport PID.
   Performs delayed fetching to ensure processes are captured.
+  When a window_id is not found for a transport, retries from the beginning
+  (re-fetching debugged_lv_processes) because the set of processes may have changed.
   """
   @spec fetch_grouped_lv_processes() ::
           {grouped_lv_processes :: grouped_lv_processes(),
            lv_processes_count :: non_neg_integer()}
   def fetch_grouped_lv_processes() do
-    lv_processes =
-      with [] <- fetch_lv_processes_after(0),
-           [] <- fetch_lv_processes_after(500) do
-        fetch_lv_processes_after(1000)
-      end
+    retries_timeouts = [200, 400, 800, 1600]
 
-    {LiveViewDiscovery.group_lv_processes(lv_processes), length(lv_processes)}
+    lv_processes =
+      Enum.reduce_while(retries_timeouts, [], fn timeout, _acc ->
+        Process.sleep(timeout)
+
+        case LiveViewDiscovery.debugged_lv_processes() do
+          [] ->
+            {:cont, []}
+
+          lv_processes ->
+            fingerprint_by_transport_pid =
+              lv_processes
+              |> Enum.group_by(& &1.transport_pid)
+              |> Enum.map(fn {transport_pid, processes} ->
+                {transport_pid, WindowsStorage.create_fingerprint(processes)}
+              end)
+
+            transport_pid_to_window_id =
+              Enum.map(fingerprint_by_transport_pid, fn {transport_pid, fingerprint} ->
+                {transport_pid, WindowsStorage.get_window_id!(fingerprint)}
+              end)
+              |> Enum.into(%{})
+
+            all_window_ids_found? =
+              Enum.all?(transport_pid_to_window_id, fn {_, window_id} -> window_id != nil end)
+
+            if all_window_ids_found? do
+              lv_processes =
+                Enum.map(
+                  lv_processes,
+                  &LvProcess.set_window_id(&1, transport_pid_to_window_id[&1.transport_pid])
+                )
+
+              {:halt, lv_processes}
+            else
+              {:cont, []}
+            end
+        end
+      end)
+
+    {
+      LiveViewDiscovery.group_lv_processes(lv_processes),
+      length(lv_processes)
+    }
   end
 
   @doc """
@@ -43,31 +83,5 @@ defmodule LiveDebugger.App.Discovery.Queries do
       |> Enum.map(&(LvProcess.new(&1.pid, &1.socket) |> LvProcess.set_alive(false)))
 
     {LiveViewDiscovery.group_lv_processes(dead_lv_processes), length(dead_lv_processes)}
-  end
-
-  defp fetch_lv_processes_after(milliseconds) do
-    Process.sleep(milliseconds)
-
-    # TODO: This is extremely inefficient...
-    lv_processes =
-      LiveViewDiscovery.debugged_lv_processes()
-      |> Enum.map(fn lv_process ->
-        fingerprint =
-          lv_process.transport_pid
-          |> LiveViewDiscovery.debugged_lv_processes()
-          |> Enum.map(fn lv_process -> lv_process.socket_id end)
-          |> Enum.sort()
-          |> Enum.join(";")
-
-        window_id = WindowsStorage.get_window_id!(fingerprint)
-        dbg(window_id)
-        LvProcess.set_window_id(lv_process, window_id)
-      end)
-
-    if Enum.find(lv_processes, fn lv_process -> lv_process.window_id == nil end) do
-      fetch_lv_processes_after(milliseconds)
-    else
-      lv_processes
-    end
   end
 end
