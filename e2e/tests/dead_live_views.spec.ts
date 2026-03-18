62fe09eeeaa1be9df4bff5677fb6d4249b039824
srzeszut <szczepan.rzeszutek@swmansion.com>
2026-03-18 12:51:55 +0100

    migrate elements inspection to playwright

--- e2e/tests/dead_live_views.spec.ts
+++ e2e/tests/dead_live_views.spec.ts
@@ -6,13 +6,16 @@ const test = prepareDevDebuggerPairTest('/');
 const deadSessionsContainer = (page: Page) => page.locator('#dead-sessions');
 const toggleDeadLiveViewsBtn = (page: Page) =>
   page.locator('div[phx-click="toggle-dead-liveviews"]');
-
-const deadSessionItems = (page: Page) =>
-  page.locator('#dead-sessions button[phx-click="select-live-view"]');
 const navbarConnected = (page: Page) => page.locator('#navbar-connected');
 
 test('dead LiveViews are available to debug', async ({ devApp, dbgApp }) => {
+  const pid = (await devApp.locator('#current-pid').textContent()) ?? '';
+
   await returnButton(dbgApp).click();
+
+  await expect(
+    dbgApp.getByRole('heading', { name: 'Active LiveViews' })
+  ).toBeVisible();
 
   if (await deadSessionsContainer(dbgApp).isVisible()) {
     await toggleDeadLiveViewsBtn(dbgApp).click();
@@ -20,17 +23,18 @@ test('dead LiveViews are available to debug', async ({ devApp, dbgApp }) => {
   await expect(deadSessionsContainer(dbgApp)).not.toBeVisible();
 
   await toggleDeadLiveViewsBtn(dbgApp).click();
-
-  await expect(deadSessionsContainer(dbgApp)).toContainText(
-    'No dead LiveViews'
-  );
+  await expect(deadSessionsContainer(dbgApp)).toBeVisible();
 
   await devApp.goto('/side');
   await devApp.goto('/');
 
-  await expect(deadSessionItems(dbgApp)).toHaveCount(2);
+  const deadPidBtn = dbgApp.locator(
+    `#dead-sessions button[id="${pid}"][phx-click="select-live-view"]`
+  );
+  await expect(deadPidBtn).toBeVisible();
+  await deadPidBtn.hover();
+  await deadPidBtn.click();
 
-  await deadSessionItems(dbgApp).first().click();
-
+  await expect(navbarConnected(dbgApp)).toBeVisible();
   await expect(navbarConnected(dbgApp)).toContainText('Disconnected');
 });
